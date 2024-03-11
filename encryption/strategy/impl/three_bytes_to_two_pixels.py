e038b7bbd383ddb7dea49a92239b547c2caea0c7
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-03-11 09:51:14 +0200

    fix encrypt bugs and exception

--- encryption/strategy/impl/three_bytes_to_two_pixels.py
+++ encryption/strategy/impl/three_bytes_to_two_pixels.py
@@ -1,6 +1,8 @@
+from collections import deque
 from typing import override
 import numpy as np
 from encryption.strategy.definition.encryption_strategy import EncryptionStrategy
+
 
 class ThreeBytesToTwoPixels(EncryptionStrategy):
     def __init__(self):
@@ -23,36 +25,51 @@ class ThreeBytesToTwoPixels(EncryptionStrategy):
         the reassembly of the a byte will be as follows:
         ByteA = P1[0][0-3] | (P2[0][0-3] >> 4 )
         """
-        # next_pixel = 0
-        # frame = np.zeros((*self.dims, 3), dtype=np.uint8)
-        # for j in range(0, len(bytes_chunk), 3):
-        #     three_bytes = bytes_chunk[j:j + 3]
-        #     # transform to two pixels
-        #     frame[next_pixel // 0, next_pixel % self.dims[0]] = three_bytes
-        #     next_pixel += 1
-        #     frame[next_pixel // 0, next_pixel % self.dims[0]] = self.transform_bytes(three_bytes)
-        #     next_pixel += 1
-        #
-        # frames_collection[i] = frame
-        frame = np.zeros((*self.dims, 3), dtype=np.uint8)
-        chunks = np.reshape(bytes_chunk, (-1, 3))
+
+        total_bytes_per_channel = np.multiply(*self.dims)
+        frame = np.zeros((total_bytes_per_channel, 3), dtype=np.uint8)
+        chunks = np.reshape(list(bytes_chunk), (-1, 3))
         transformed_chunks = np.apply_along_axis(self.transform_bytes, 1, chunks)
-        pixels = transformed_chunks.flatten()
-        frame[:pixels.shape[0]] = np.reshape(pixels, frame.shape)
-        frames_collection[i] = frame
+        combined_chunks = self.interleaving_two_np_arrays(chunks, transformed_chunks)
+        pixels = combined_chunks.reshape((-1, 3))
+        frame[:pixels.shape[0]] = pixels
+        frames_collection[i] = frame.reshape((total_bytes_per_channel, 3))
+
+
+
+
     @override
     def decrypt(self, bytes_amount_to_read, encrypted_frame, bytes_collection, i):
-        pass
+        pixels = encrypted_frame.flatten()
+        bytes_queue = deque()
+        for pixel in pixels:
+            bytes_queue.append(pixel >> 4)  # Get the leftmost 4 bits
+            bytes_queue.append(pixel & 0x0F)  # Get the rightmost 4 bits
+        bytes_list = []
+        while bytes_queue:
+            byte1 = bytes_queue.popleft()
+            byte2 = bytes_queue.popleft()
+            byte3 = bytes_queue.popleft()
+            original_byte = (byte1 << 4) | byte2
+            bytes_list.extend([original_byte, byte3])
+        decrypted_bytes = bytes(bytes_list)[:bytes_amount_to_read]
+        bytes_collection[i] = decrypted_bytes
+
+    # def transform_bytes(self, three_bytes):
+    #     print("before",three_bytes)
+    #     # three_bytes[1]=self.transform_byte(three_bytes[1])
+    #     after=np.apply_along_axis(self.transform_byte, 0, three_bytes)
+    #     print("after",after)
+    #     return after
+    def interleaving_two_np_arrays(self, np_array1: np.array, np_array2: np.array):
+        return np.ravel(np.column_stack((np_array1, np_array2)))
 
     def transform_bytes(self, three_bytes):
-        return np.apply_along_axis(self.transform_byte, 0, three_bytes)
+        leftmost = (three_bytes & 0xF0) >> 4
+        rightmost = (three_bytes & 0x0F) << 4
 
-    def transform_byte(self, byte):
-        leftmost = (byte & 0xF0) >> 4
-        rightmost = (byte & 0x0F) << 4
         return rightmost | leftmost
 
 
-
 PROTO_3B_TO_2PIX = ThreeBytesToTwoPixels()
-print(PROTO_3B_TO_2PIX.transform_byte(0b11010000))
+# print(PROTO_3B_TO_2PIX.transform_bytes(0b11010000))
