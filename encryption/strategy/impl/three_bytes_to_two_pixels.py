10684c6e9ca9f06653627a37beb0b003a615f037
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-03-13 21:51:20 +0200

    remove time logging for eact step

--- encryption/strategy/impl/three_bytes_to_two_pixels.py
+++ encryption/strategy/impl/three_bytes_to_two_pixels.py
@@ -32,55 +32,17 @@ class ThreeBytesToTwoPixels(EncryptionStrategy):
         ByteA = P1[0][0-3] | (P2[0][0-3] >> 4 )
         """
 
-        # total_bytes_per_channel = np.multiply(*self.dims)
-        # frame = np.zeros((total_bytes_per_channel, 3), dtype=np.uint8)
-        # chunks = np.reshape(list(bytes_chunk), (-1, 3))
-        # transformed_chunks = np.apply_along_axis(self.transform_bytes, 1, chunks)
-        # combined_chunks = self.interleaving_two_np_arrays(chunks, transformed_chunks)
-        # pixels = combined_chunks.reshape((-1, 3))
-        # frame[:pixels.shape[0]] = pixels
-        # frames_collection[i] = frame.reshape((self.dims[1], self.dims[0], 3))
         print(f" ------------------ ENCRYPT frame id: {i} ------------------")
-        start_time_total = start_time = time.time()
+        start_time = time.time()
         total_bytes_per_channel = np.multiply(*self.dims)
         frame = np.zeros((total_bytes_per_channel, 3), dtype=np.uint8)
-        total_time = time.time() - start_time
-        print(f"1-Time taken for creating 'frame':", total_time)
-
-        start_time = time.time()
         chunks = np.reshape(list(bytes_chunk), (-1, 3))
-        total_time = time.time() - start_time
-        print(f"2-Time taken for reshaping 'bytes_chunk':{total_time:.4f} sec")
-
-        # start_time = time.time()
-        # transformed_chunks = np.apply_along_axis(self.transform_bytes, 1, chunks)
-        # total_time = time.time() - start_time
-        # print(f"3-Time taken for applying transformation:{total_time:.4f} sec")
-        start_time = time.time()
         transformed_chunks = self.swap_right_left_bits(chunks)
-        total_time = time.time() - start_time
-        print(f"3-Time taken for applying transformation :{total_time:.4f} sec")
-        start_time = time.time()
         combined_chunks = self.interleaving_two_np_arrays(chunks, transformed_chunks)
-        total_time = time.time() - start_time
-        print(f"4-Time taken for interleaving:{total_time:.4f} sec")
-
-        start_time = time.time()
         pixels = combined_chunks.reshape((-1, 3))
-        total_time = time.time() - start_time
-        print(f"5-Time taken for reshaping 'combined_chunks':{total_time:.4f} sec")
-
-        start_time = time.time()
         frame[:pixels.shape[0]] = pixels
-        total_time = time.time() - start_time
-        print(f"6-Time taken for assigning 'pixels' to 'frame':{total_time:.4f} sec")
-
-        start_time = time.time()
         frames_collection[i] = frame.reshape((self.dims[1], self.dims[0], 3))
         total_time = time.time() - start_time
-        print(f"7-Time taken for reshaping 'frame' and assigning to 'frames_collection':{total_time:.4f} sec")
-
-        total_time = time.time() - start_time_total
         print(f" total time to process frame {i}:{total_time:.4f} sec")
         self.total_frame_number += 1
 
@@ -93,24 +55,14 @@ class ThreeBytesToTwoPixels(EncryptionStrategy):
         # bytes_collection[i] = reconstruct_frame_bytes.reshape(-1)[:bytes_amount_to_read]
         print(f" ------------------ DENCRYPT frame id: {i} ------------------")
 
-        start_time_total = start_time = time.time()
+        start_time = time.time()
         pixel_flatten = encrypted_frame.reshape((-1, 3))
         frame_2chucks_group = encrypted_frame.reshape((-1, 2, 3))
-        total_time = time.time() - start_time
-        print(f"Time taken for reshaping 'encrypted_frame':{total_time:.4f} sec")
-        start_time = time.time()
         original_pixels = pixel_flatten[::2]
         reversed_pixels = pixel_flatten[1::2]
         reconstruct_frame_bytes = np.array(self.construct_byte_from_2_pixels(original_pixels, reversed_pixels))
-        total_time = time.time() - start_time
-        print(f"Time taken for reconstructing frame bytes v2:{total_time:.4f} sec")
-
-        start_time = time.time()
         bytes_collection[i] = reconstruct_frame_bytes.reshape(-1)[:bytes_amount_to_read]
         total_time = time.time() - start_time
-        print(f"Time taken for reshaping and assigning to 'bytes_collection':{total_time:.4f} sec")
-
-        total_time = time.time() - start_time_total
         print(f" total time to process frame {i}:{total_time:.4f} sec")
 
     def interleaving_two_np_arrays(self, np_array1: np.array, np_array2: np.array):
