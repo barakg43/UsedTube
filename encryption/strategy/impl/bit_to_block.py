d521d216aec03e3ea37005e0d1c498b982bec13c
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-04-02 08:10:37 +0300

    add fourcc,file_ext to ctor BitToBlock

--- encryption/strategy/impl/bit_to_block.py
+++ encryption/strategy/impl/bit_to_block.py
@@ -1,14 +1,15 @@
 import numpy as np
+
 from encryption.constants import MAX_UINT8
 from encryption.strategy.definition.encryption_strategy import EncryptionStrategy
 
 
 class BitToBlock(EncryptionStrategy):
-    def __init__(self, block_size=4):
-        super().__init__()
+    def __init__(self, block_size=4, fourcc: str = "RBGA", out_format: str = "avi"):
+        super().__init__(fourcc, out_format)
+
         self.block_size = block_size
         self.bytes_2_pixels_ratio = 8 * block_size ** 2
-
 
     def encrypt(self, bytes_chunk, frames_collection, i):
         bits = np.unpackbits(np.frombuffer(bytes_chunk, dtype=np.uint8))
@@ -29,9 +30,8 @@ class BitToBlock(EncryptionStrategy):
         num_blocks = blocks_arr.shape[0]
         for row, col, idx in self.position_generator(num_blocks, width):
             frame[row: row + block_size, col:col + block_size] = blocks_arr[idx]
-        return frame
+        # return frame
         frames_collection[i] = frame
-
 
     def __calculate_position(self, block, width):
         block_size = self.block_size
@@ -57,7 +57,6 @@ class BitToBlock(EncryptionStrategy):
         )
         decoded_bits = np.array(list(map(lambda block: 1 if np.mean(block) > 127 else 0, blocks)), dtype=np.uint8)
         bytes_collection[i] = np.packbits(decoded_bits)
-
 
 # btb = BitToBlock()
 # frame = btb.encrypt(b'GAL AVIEZRI G.AVIEZRI@gmail.com', [], 0)
