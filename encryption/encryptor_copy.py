c1d9a380c90f92972f615409742f826c1c8a12f9
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-04-05 10:35:29 +0300

    add copys of original code file

---
+++ encryption/encryptor_copy.py
@@ -0,0 +1,147 @@
+import concurrent.futures
+import hashlib
+import logging
+import os
+from concurrent.futures import ThreadPoolExecutor
+from typing import IO
+
+import cv2
+import numpy as np
+
+from encryption.constants import ENCRYPT_LOGGER, DECRYPT_LOGGER
+from encryption.strategy.definition.encryption_strategy import EncryptionStrategy
+
+encrypt_logger = logging.getLogger(ENCRYPT_LOGGER)
+decrypt_logger = logging.getLogger(DECRYPT_LOGGER)
+"""
+CHUNK_SIZE SHOULD BE CALCULATED ONCE, BASED ON ENCRYPTION METHOD WE WILL CHOOSE
+IF ONLY ONE COVER VIDEO IS USED, METADATA SHOULD ALSO BE RETRIEVED ONCE
+"""
+
+
+class Encryptor:
+    def __init__(self, strategy: EncryptionStrategy):
+        self.file_size: int = 0
+        self.encoding: int = 0
+        self.fps: int = 0
+        self.chunk_size: int = 0
+        self.strategy: EncryptionStrategy = strategy
+        self.workers: ThreadPoolExecutor = ThreadPoolExecutor(25)  # Arbitrary; Inspired by FPS
+        self.enc_logger = logging.getLogger(ENCRYPT_LOGGER)
+        self.dec_logger = logging.getLogger(DECRYPT_LOGGER)
+        self.original_sha256 = ""
+
+    def get_cover_video_metadata(self, cover_video):
+        self.strategy.dims = (
+            int(cover_video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cover_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        )
+        self.fps = cover_video.get(cv2.CAP_PROP_FPS)
+        self.encoding = cv2.VideoWriter.fourcc(*self.strategy.fourcc)
+        self.strategy.dims_multiplied = np.multiply(*self.strategy.dims)
+
+    def collect_metadata(self, file_br, cover_video):
+        self.get_cover_video_metadata(cover_video)
+        fd = file_br.fileno()
+        self.file_size = os.fstat(fd).st_size
+
+    def encrypt(self, file_to_encrypt: IO, cover_video_path: str, out_vid_path: str):
+        """"
+        :param out_vid_path: string path
+        :param cover_video_path:
+        :parameter file_to_encrypt an open file descriptor in 'rb' to the file
+        """
+
+        cover_video = cv2.VideoCapture(cover_video_path)
+        self.collect_metadata(file_to_encrypt, cover_video)
+
+        if self.chunk_size == 0:
+            self.enc_logger.debug("calculating chunk size")
+            self.chunk_size = self.strategy.calculate_chunk_size()
+
+        encrypted_frames = np.empty(int(np.ceil(self.file_size / self.chunk_size)), dtype=object)
+        futures = np.empty(int(np.ceil(self.file_size / self.chunk_size)), dtype=concurrent.futures.Future)
+        self.enc_logger.debug(f"about to process {len(futures)} chunks")
+        # read chunks sequentially and start strategy.encrypt
+        bytes_chunk = file_to_encrypt.read(self.chunk_size)
+
+        chunk_number = 0
+        while bytes_chunk:
+            # strategy.encrypt returns an encrypted frame
+            # futures[chunk_number] = self.workers.submit(self.strategy.encrypt, bytes_chunk, encrypted_frames,
+            #                                             chunk_number)
+            #  use encrypt without ThreadPool
+            futures[chunk_number] = self.strategy.encrypt(bytes_chunk, encrypted_frames,
+                                                          chunk_number)
+            # read next chunk
+            chunk_number += 1
+            self.enc_logger.debug(f"encryptor submitted chunk {bytes_chunk} number #{chunk_number} for encryption")
+            bytes_chunk = file_to_encrypt.read(self.chunk_size)
+
+        self.enc_logger.debug(f"total of {chunk_number} chunks were submitted to workers")
+
+        # wait(futures)
+        self.enc_logger.debug("waiting for workers to finish processing chunks...")
+        output_video = cv2.VideoWriter(out_vid_path, self.encoding, self.fps, self.strategy.dims)  # TODO: fix encoding
+        for frame in encrypted_frames:
+            output_video.write(frame)
+
+        # Closes all the video sources
+        cover_video.release()
+        output_video.release()
+        cv2.destroyAllWindows()
+
+    def generateSha256ForFile(self, file_bytes: IO):
+        file_bytes.seek(0)
+        sha256Hashed = hashlib.file_digest(file_bytes, 'sha256').hexdigest()
+        file_bytes.seek(0)
+        return sha256Hashed
+
+    def decrypt(self, encrypted_file_as_video_path, file_size, decrypted_file):
+        # CHANGE THE IO OPERATIONS TO BE SEQUENTIAL
+        # IMPLEMENT CONCURRENCY HERE ASWELL
+
+        enc_file_videocap = cv2.VideoCapture(encrypted_file_as_video_path)
+        self.get_cover_video_metadata(enc_file_videocap)
+
+        bytes_left_to_read = file_size
+
+        decrypted_bytes = np.empty(int(np.ceil(self.file_size / self.chunk_size)), dtype=object)
+        futures = np.empty(int(np.ceil(self.file_size / self.chunk_size)), dtype=concurrent.futures.Future)
+
+        if self.chunk_size is None:
+            self.dec_logger.debug("calculating chunk size...")
+            self.chunk_size = self.strategy.calculate_chunk_size()
+        self.dec_logger.debug(f"about to process {len(futures)} frames")
+        frame_number = 0
+        while bytes_left_to_read > 0:
+            ret, encrypted_frame = enc_file_videocap.read()
+            if not ret:
+                break
+
+            bytes_amount_to_read = self.calculate_bytes_amount_to_read(bytes_left_to_read)
+            print(f"chunk {bytes_amount_to_read} bytes #{frame_number} for decryption")
+            bytes_left_to_read -= bytes_amount_to_read
+            #  use decrypt without ThreadPool
+            futures[frame_number] = self.strategy.decrypt(bytes_amount_to_read, encrypted_frame,
+                                                          decrypted_bytes, frame_number)
+            # futures[frame_number] = self.workers.submit(self.strategy.decrypt, bytes_amount_to_read, encrypted_frame,
+            #                                             decrypted_bytes, frame_number)
+            self.dec_logger.debug(
+                f"encryptor submitted chunk {bytes_amount_to_read} bytes #{frame_number} for decryption")
+            print(
+                f"encryptor submitted chunk {bytes_amount_to_read} bytes #{frame_number} for decryption")
+            frame_number += 1
+        self.enc_logger.debug(f"total of {frame_number} frames were submitted to workers")
+
+        # wait(futures)
+
+        for _bytes in decrypted_bytes:
+            decrypted_file.write(bytes(_bytes.tolist()))
+
+        enc_file_videocap.release()
+        cv2.destroyAllWindows()
+
+    def calculate_bytes_amount_to_read(self, bytes_left_to_read):
+        bytes_amount_in_frame = self.strategy.dims_multiplied / self.strategy.bytes_2_pixels_ratio
+        bytes_amount_to_read = self.chunk_size if bytes_left_to_read > bytes_amount_in_frame else bytes_left_to_read
+        return bytes_amount_to_read
---
+++ encryption/test_encryptor_copy.py
@@ -0,0 +1,89 @@
+import os
+import time
+import unittest
+from pathlib import Path
+
+import numpy as np
+
+from encryption.encryptor import Encryptor
+from encryption.strategy.impl.bit_to_block import BitToBlock
+from encryption.strategy.impl.one_byte_to_one_pixel import PROTO_1B_1P
+from encryption.strategy.impl.three_bytes_to_two_pixels import ThreeBytesToTwoPixels
+
+RESOURCES_DIR = Path('../resources/')
+OUTPUT_DIR = Path('../output_files/')
+PDF_PATH = 0
+ENC_OUT_VID_PATH = 1
+COVER_VID_PATH = 2
+DEC_PDF_PATH = 3
+
+
+class EncryptorTest(unittest.TestCase):
+
+    def paths_dict(self):
+        original_file_ext = "pdf"
+        paths_dict = {PDF_PATH: (RESOURCES_DIR / f"sample-file.{original_file_ext}").as_posix(),
+                      ENC_OUT_VID_PATH: (
+                              OUTPUT_DIR / f"output-video_{self.enc.strategy.fourcc}.{self.enc.strategy.out_format}")
+                      .as_posix(),
+                      COVER_VID_PATH: (RESOURCES_DIR / "sample.mp4").as_posix(),
+                      DEC_PDF_PATH: (
+                              OUTPUT_DIR / f"sample-file-decrypted_{self.enc.strategy.fourcc}.{original_file_ext}").as_posix()}
+
+        return paths_dict
+
+    def check_pdf_encryption(self, proto):
+        self.enc = Encryptor(proto)
+        paths = self.paths_dict()
+        pdf_file = open(paths[PDF_PATH], 'rb')
+
+        decrypted_pdf_file = open(paths[DEC_PDF_PATH], "wb+")
+        begin_time = time.time()
+        self.enc.encrypt(pdf_file, paths[COVER_VID_PATH], paths[ENC_OUT_VID_PATH])
+        end_time = time.time()
+        print(f"Encoded In {end_time - begin_time}")
+        begin_time = time.time()
+        file_size = os.stat(paths[PDF_PATH]).st_size
+        self.enc.decrypt(paths[ENC_OUT_VID_PATH], file_size, decrypted_pdf_file)
+        end_time = time.time()
+        print(f"Decoded In {end_time - begin_time}")
+        original_sha256 = self.enc.generateSha256ForFile(pdf_file)
+        decrypted_sha256 = self.enc.generateSha256ForFile(decrypted_pdf_file)
+        input_content = pdf_file.read()
+        output_content = decrypted_pdf_file.read()
+        res = True
+        bytes_asserted = 0
+        for (byte1, byte2) in zip(input_content, output_content):
+            if byte1 != byte2:
+                bytes_asserted += 1
+                if (bytes_asserted < 9):
+                    print(f"input: {np.binary_repr(byte1, width=8)}, output: {np.binary_repr(byte2, width=8)}")
+                    # break
+        print(f"bytes asserted: {bytes_asserted}")
+
+        pdf_file.close()
+        decrypted_pdf_file.close()
+
+        return original_sha256, decrypted_sha256
+
+    def perform_test_3P_2B(self, codec, file_ext):
+        # Replace this with your actual test implementation
+        print(f"#### 3P_2B: Testing codec '{codec}' with file extension '.{file_ext}' ###")
+        sha256_1, sha256_2 = self.check_pdf_encryption(ThreeBytesToTwoPixels(fourcc=codec, out_format=file_ext))
+        print(sha256_1)
+        print(sha256_2)
+        self.assertEqual(sha256_1, sha256_2)
+
+    def perform_test_1Bit_Block(self, codec, file_ext):
+        # Replace this with your actual test implementation
+        print(f"#### Bit to Block: Testing codec '{codec}' with file extension '.{file_ext}' ###")
+        sha256_1, sha256_2 = self.check_pdf_encryption(BitToBlock(block_size=8, fourcc=codec, out_format=file_ext))
+        print(sha256_1)
+        print(sha256_2)
+        self.assertEqual(sha256_1, sha256_2)
+
+    def test_encryptor_pdf_1B_1P(self):
+        self.check_pdf_encryption(PROTO_1B_1P)
+
+    def test_encryptor_pdf_3B_2P(self):
+        self.assertEqual(*self.check_pdf_encryption(ThreeBytesToTwoPixels(fourcc="RGBA", out_format="avi")))
