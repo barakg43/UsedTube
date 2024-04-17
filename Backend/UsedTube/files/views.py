536f6069ed43c60794799108396f6ece78d09b95
Gal Aviezri <g.aviezri@gmail.com>
2024-04-17 14:18:45 +0300

    reinit server

--- Backend/UsedTube/files/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-
-def handle_file_download(request: HttpRequest):
-    return HttpResponse('hello world!')
