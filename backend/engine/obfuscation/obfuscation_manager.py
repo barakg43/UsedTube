cad58b9b6fd37b33ed349c2e4f6748cfc79d0dac
Gal Aviezri <g.aviezri@gmail.com>
2024-04-18 20:52:22 +0300

    created query.py which is where all queries will be implemented.

--- backend/engine/obfuscation/obfuscation_manager.py
+++ backend/engine/obfuscation/obfuscation_manager.py
@@ -2,19 +2,39 @@
 
 class ObfuscationManager:
 
-    def __init__(self, intermeshing_cycle=1):
+    def __init__(self, intermeshing_cycle: int=1):
         """
         Mix serialized frames with frames of a real video inorder to make
         the serialized videos hard to notice.
         first frame of uploaded videos are always of the serialized file.
         :param intermeshing_cycle: the amount of cover video frames between 2
         serialized frames.
+        NOTE: 0th frame is always of the encrypted data
         """
         self.cycle = intermeshing_cycle
 
 
-    def obfuscate(self, file_frames_path, cover_video_path) -> str:
+    def obfuscate(self, file_frames_path: str, cover_video_path: str) -> str:
+        # open 2 video as streams file_frames as ff, cover_video as cov
+        # create new video container as out
+        # do:
+        #   read frame from ff
+        #   write frame to out
+        #   read {self.cycle} frames from cov
+        #   write {self.cycle} frames to out
+        #   while file_frames got frames
+        # save out to a generated path, using UUID to avoid collisions?
+        # return the path
         pass
 
-    def untangle(self, obfuscated_video) -> str:
+    def untangle(self, obfuscated_video_path:str ) -> str:
+        # open the obfuscated_video as obsv
+        # create a new out video container as out
+        # do:
+        #   read frame from obsv
+        #   write that frame to out
+        #   skip {self.cycle} frames from obsv
+        #   while obsv got frames
+        # save out to a generated path, using UUID to avoid collisions?
+        # return the path
         pass
