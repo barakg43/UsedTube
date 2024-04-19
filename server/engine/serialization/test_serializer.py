a67e53acc517f8479b1e28bd8fbdcc5a414b72e4
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-04-19 18:51:54 +0300

    add log of finish test

--- server/engine/serialization/test_serializer.py
+++ server/engine/serialization/test_serializer.py
@@ -9,8 +9,10 @@ from server.engine.serialization.serializer import Serializer
 from server.engine.serialization.strategy.impl.bit_to_block import BitToBlock
 from server.engine.serialization.strategy.impl.three_bytes_to_two_pixels import ThreeBytesToTwoPixels
 
-RESOURCES_DIR = Path('../server/engine/resources/')
-OUTPUT_DIR = Path('../server/engine/output_files')
+
+RESOURCES_DIR = Path('server/engine/resources/')
+OUTPUT_DIR = Path('server/engine/output_files')
+
 if not os.path.exists(OUTPUT_DIR):
     os.mkdir(OUTPUT_DIR)
 PDF_PATH = 0
@@ -110,6 +112,8 @@ class SerializerTest(unittest.TestCase):
         print(f"#### Bit to Block: Testing codec '{codec}' with file extension '.{file_ext}' ###")
         sha256_1, sha256_2 = self.check_pdf_serialization(BitToBlock(fourcc=codec, out_format=file_ext, block_size=4))
         self.assertEqual(sha256_1, sha256_2)
+        print(f"$$$$ Bit to Block: Testing codec '{codec}' PASS $$$$")
+
 
     def perform_test_youtube_1B_1Block_decryption(self, codec, file_ext, encrypted_video_path, original_file_path):
         # Replace this with your actual test implementation
