d3e9a8a92830c552a267fbd344a08634d91a2c59
omriSR <omri.1996s@gmail.com>
2024-04-19 16:59:22 +0300

    obfuscate function - untested

--- backend/engine/obfuscation/obfuscation_manager.py
+++ backend/engine/obfuscation/obfuscation_manager.py
@@ -1,4 +1,5 @@
-
+import cv2 
+import logging
 
 class ObfuscationManager:
 
@@ -12,7 +13,8 @@ class ObfuscationManager:
         NOTE: 0th frame is always of the encrypted data
         """
         self.cycle = intermeshing_cycle
-
+        self.logger = logging.Logger(__name__)
+        self.logger.setLevel(logging.INFO)
 
     def obfuscate(self, file_frames_path: str, cover_video_path: str) -> str:
         # open 2 video as streams file_frames as ff, cover_video as cov
@@ -25,7 +27,49 @@ class ObfuscationManager:
         #   while file_frames got frames
         # save out to a generated path, using UUID to avoid collisions?
         # return the path
-        pass
+
+        ff = cv2.VideoCapture(file_frames_path)
+        assert ff.isOpened(), "Could not open file_frames_path"
+
+        cov = cv2.VideoCapture(cover_video_path)
+        assert cov.isOpened(), "Could not open cover_video_path"
+
+        # Get video properties
+        fps = ff.get(cv2.CAP_PROP_FPS)
+        width = int(ff.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(ff.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        out_path = "output_video.mp4" 
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter(out_path, fourcc, fps, (width, height))
+
+        frame_count = 0
+
+        
+        while True:
+            ret_ff, frame_ff = ff.read()
+            if not ret_ff:
+                self.logger.info("loop ended. No more frames in file_frames")
+                break  
+
+            out.write(frame_ff)  
+            frame_count += 1
+
+            for _ in range(self.cycle):
+                ret_cov, frame_cov = cov.read()
+                if not ret_cov:
+                    self.logger.info("loop ended. No more frames in cover_video")
+                    break  
+
+                out.write(frame_cov)  
+
+        # Release video capture and writer
+        ff.release()
+        cov.release()
+        out.release()
+
+        return out_path
+
 
     def untangle(self, obfuscated_video_path:str ) -> str:
         # open the obfuscated_video as obsv
