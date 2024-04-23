a2f2a84368afad107c59036993a6b13dcfb05c79
Gaviezri <g.aviezri@gmail.com>
2024-04-23 14:04:27 +0300

    in constants.py: implemented a loop to create all basic necessary directories.
    in files.views.py:
    started implementing the retrieval of a stored file logic

--- backend/files/views.py
+++ backend/files/views.py
@@ -9,15 +9,16 @@ from engine.downloader.definition import Downloader
 from files.query import get_items_in_folder
 from engine.driver import Driver
 
+
 class Download(View):
     def get(self, request: HttpRequest):
         # receive user request to download file
         # you get in request: user id, file_name
         # from the db extract video_url, compressed_file_size
-        compressed_file_size = 10 # in Bytes
+        compressed_file_size = 10  # in Bytes
         video_url = r"https://www.youtube.com/watch?v=0EqSXDwTq6U&ab_channel=jasminmakeup"
         # use the downloader to download the video from url
-        downloader: type(Downloader) = YouTubeDownloader # choose based on URL
+        downloader: type(Downloader) = YouTubeDownloader  # choose based on URL
         downloaded_video_path = downloader.download(video_url)
         restored_file_path = Driver.process_video_to_file(downloaded_video_path, compressed_file_size)
         pass
