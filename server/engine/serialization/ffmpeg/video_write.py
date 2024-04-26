457f4f0b99071af27ca7156bde6af6b95aba719b
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-04-26 17:56:02 +0300

    move VideoWriter, VideoCapture to ffmpeg folder

--- server/engine/serialization/ffmpeg/video_write.py
+++ server/engine/serialization/ffmpeg/video_write.py
@@ -2,13 +2,13 @@ import ffmpeg
 
 
 class VideoWriter:
-    def __init__(self, out_video_path, fourcc: str, fps: int, frame_size: (int, int), logging_stdout=False):
+    def __init__(self, out_video_path, fourcc: str, fps: int, bitrate: int, frame_size: (int, int),
+                 logging_stdout=False):
         self.path = out_video_path
-        kbps = 1024
         self.output_video = (
             ffmpeg.input(filename='pipe:', format='rawvideo', pix_fmt='bgr24',
                          s='{}x{}'.format(frame_size[0], frame_size[1]))
-            .output(out_video_path, r=f'{fps}', vtag=fourcc, video_bitrate=8000 * kbps)
+            .output(out_video_path, r=f'{fps}', vtag=fourcc)
             .overwrite_output()
             .run_async(pipe_stdin=True)
         )
