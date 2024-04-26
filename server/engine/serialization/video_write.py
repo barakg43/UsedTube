5d7f806d7a176b84ac9655776bea2c92a491685a
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-04-26 13:33:19 +0300

    create VideoWriter class from ffmpeg  libaray to replace cv2

---
+++ server/engine/serialization/video_write.py
@@ -0,0 +1,21 @@
+import ffmpeg
+
+
+class VideoWriter:
+    def __init__(self, out_video_path, fourcc: str, fps: int, frame_size: (int, int), logging_stdout=False):
+        self.path = out_video_path
+        self.output_video = (
+            ffmpeg.input(filename='pipe:', format='rawvideo', pix_fmt='bgr24',
+                         s='{}x{}'.format(frame_size[0], frame_size[1]))
+            .output(out_video_path, r=f'{fps}', vtag=fourcc, video_bitrate=1430000)
+            .overwrite_output()
+            .run_async(pipe_stdin=True)
+        )
+
+    def write(self, frame):
+        self.output_video.stdin.write(frame.tobytes())
+
+    def release(self):
+        self.output_video.stdin.close()
+        # self.output_video.stdout.close()
+        self.output_video.wait()
