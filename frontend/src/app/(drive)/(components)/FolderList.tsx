d4da16252aeae89700930c54ec1ce5e1c956b9bc
Gal Aviezri <g.aviezri@gmail.com>
2024-05-04 14:02:46 +0300

    restructuring

--- frontend/src/app/(drive)/(components)/FolderList.tsx
+++ frontend/src/app/(drive)/(components)/FolderList.tsx
@@ -1,5 +1,6 @@
-import FolderItem from "@/src/app/drive/(components)/FolderItem";
+
 import { Divider, List } from "@mui/material";
+import FolderItem from "./FolderItem";
 
 function FolderList() {
   return (
--- frontend/src/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <h1>Welcome Home</h1>;
-}
