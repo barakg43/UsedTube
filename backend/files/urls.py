a5191b29454989693fa5227ba47890a53f0a4a5b
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-05 07:25:36 +0300

    add a path variable to 'dir-content'

--- backend/files/urls.py
+++ backend/files/urls.py
@@ -5,5 +5,5 @@ urlpatterns =[
     path('download', DownloadView.as_view()),
     path('upload', UploadView.as_view()),
     path('used-space', UsedSpaceView.as_view(), name='used_space'),
-    path('dir-content', DirectoryContentView.as_view())
+    path('dir-content/<int:id>', DirectoryContentView.as_view())
 ]
