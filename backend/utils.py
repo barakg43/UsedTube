3b101cf720d43f73953671fa7f394acda6a9c653
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-05 20:46:59 +0300

    add convert_body_json_to_dict in utils

--- backend/utils.py
+++ backend/utils.py
@@ -1,2 +1,13 @@
+import json
+
+from django.http import HttpRequest
+
+
 def already_exists(what: str) -> str:
     return f"{what} already exists"
+
+
+def convert_body_json_to_dict(request: HttpRequest) -> dict:
+    body_unicode = request.body.decode('utf-8')
+    body_dict = json.loads(body_unicode)
+    return body_dict
