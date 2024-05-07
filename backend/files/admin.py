22a12683939ed2367c9ce3289102d7c841a1d6fd
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-07 23:15:29 +0300

    update field names in admin Files

--- backend/files/admin.py
+++ backend/files/admin.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
-from .models import *
 # Register your models here.
 from django.contrib import admin
+
 from .models import Folder, File, SharedItem
+
 
 @admin.register(Folder)
 class FolderAdmin(admin.ModelAdmin):
@@ -11,6 +11,7 @@ class FolderAdmin(admin.ModelAdmin):
     list_filter = ('owner', 'created_at')
     raw_id_fields = ('parent', 'owner')
 
+
 @admin.register(File)
 class FileAdmin(admin.ModelAdmin):
     list_display = ('name', 'extension', 'size', 'folder', 'owner', 'created_at', 'updated_at')
@@ -18,9 +19,10 @@ class FileAdmin(admin.ModelAdmin):
     list_filter = ('folder', 'owner', 'created_at')
     raw_id_fields = ('folder', 'owner')
 
+
 @admin.register(SharedItem)
 class SharedItemAdmin(admin.ModelAdmin):
-    list_display = ('item', 'shared_with', 'created_at')
+    list_display = ('folder_item','file_item', 'shared_with', 'shared_at')
     search_fields = ('item__name', 'shared_with__username')
     list_filter = ('shared_with',)
-    raw_id_fields = ('item', 'shared_with')
+    raw_id_fields = ('folder_item', 'file_item', 'shared_with')
