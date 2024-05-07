8f4a3670c7ba6c1304f8a5f8af35253a3e82f8fa
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-07 23:15:51 +0300

    use tailwind in FolderList

--- frontend/src/app/drive/FolderList.tsx
+++ frontend/src/app/drive/FolderList.tsx
@@ -1,9 +1,10 @@
 import FolderItem from "@/src/components/FolderItem";
 import { Divider, List } from "@mui/material";
+// sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
 
 function FolderList() {
   return (
-    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+    <List className={"w-100"}>
       <FolderItem />
 
       <FolderItem />
