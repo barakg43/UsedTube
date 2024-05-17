b719278744533a5d2234a837cf9e99f79ea3bc95
Gal Aviezri <g.aviezri@gmail.com>
2024-05-18 00:34:34 +0300

    persist store work

--- frontend/src/app/StoreProvider.tsx
+++ frontend/src/app/StoreProvider.tsx
@@ -4,6 +4,7 @@ import { FC } from "react";
 import { Provider } from "react-redux";
 import { persistStore } from "redux-persist";
 
+persistStore(store);
 const StoreProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
     return <Provider store={store}>{children}</Provider>;
 };
