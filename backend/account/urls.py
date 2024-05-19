e2ce7dd688e0e6908e868f06694fc08f4ce2c880
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-20 00:10:43 +0300

    create new jwt views

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,10 +1,18 @@
+from django.urls import include
 from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
+
 from .views import *
 
+#
 urlpatterns = [
     path('register', Register.as_view()),
+    path('auth/', include('djoser.urls')),
+
+    path("auth/jwt/create",CustomTokenObtainPairView.as_view()),
+    path("auth/jwt/refresh",CustomTokenRefreshView.as_view()),
+    path("auth/jwt/verify",CustomTokenVerifyView.as_view()),
+
     path('login', Login.as_view(), name='login'),
-    path('logout', Logout.as_view(), name='logout'),
+    path('auth/logout', LogoutView.as_view(), name='logout'),
     path('validate', Validate.as_view()),
 ]
