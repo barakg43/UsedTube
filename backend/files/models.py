de23090217e326bfb2cf962c295b3679470a5268
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-20 00:40:58 +0300

    disable    'django.middleware.csrf.CsrfViewMiddleware',

--- backend/files/models.py
+++ backend/files/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
 class Folder(models.Model):
     name = models.CharField(max_length=255)
     id = models.UUIDField(primary_key=True)
