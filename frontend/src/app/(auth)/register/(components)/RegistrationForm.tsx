6cdaad681f866d1ddbcf6c96e8e3dc8fd59177f4
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-22 00:44:14 +0300

    refactor white space 2 files

--- frontend/src/app/(auth)/register/(components)/RegistrationForm.tsx
+++ frontend/src/app/(auth)/register/(components)/RegistrationForm.tsx
@@ -6,208 +6,181 @@ import { TextField, IconButton, Button, Typography } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { UserValues } from "../../../../types";
 import { schema } from "../registration-schema";
-import { validateNotExisting } from "@/redux/api";
+import { validateNotExisting } from "@/redux/slices/api";
 import {
-    confirmPassword,
-    email,
-    firstName,
-    lastName,
-    password,
-    username,
+  confirmPassword,
+  email,
+  firstName,
+  lastName,
+  password,
+  username,
 } from "@/constants";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { setFormData } from "@/redux/slices/userSlice";
 import { RootState } from "@/redux/store";
 
 const RegistrationForm: React.FC<{ setIsFinishFillingForm: Function }> = ({
-    setIsFinishFillingForm,
+  setIsFinishFillingForm,
 }) => {
-    const [showPassword, setShowPassword] = useState(false);
-    const dispatch = useAppDispatch();
-    const apiKey = useAppSelector((s: RootState) => s.user.apiKey);
+  const [showPassword, setShowPassword] = useState(false);
+  const dispatch = useAppDispatch();
+  const apiKey = useAppSelector((s: RootState) => s.user.apiKey);
 
-    const {
-        handleSubmit,
-        formState: { errors },
-        control,
-        setError,
-    } = useForm<UserValues>({
-        //@ts-ignore
-        resolver: yupResolver<UserValues>(schema),
-    });
+  const {
+    handleSubmit,
+    formState: { errors },
+    control,
+    setError,
+  } = useForm<UserValues>({
+    //@ts-ignore
+    resolver: yupResolver<UserValues>(schema),
+  });
 
-    const onBlur = async (
-        e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement, Element>
-    ) => {
-        const results = await validateNotExisting(
-            e.target.name,
-            e.target.value
-        );
-        if (!results.valid) {
-            //@ts-ignore
-            setError(e.target.name, {
-                type: "manual",
-                message: results.message,
-            });
-        }
-    };
+  const onBlur = async (
+    e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement, Element>
+  ) => {
+    const results = await validateNotExisting(e.target.name, e.target.value);
+    if (!results.valid) {
+      //@ts-ignore
+      setError(e.target.name, {
+        type: "manual",
+        message: results.message,
+      });
+    }
+  };
 
-    const onSubmit: SubmitHandler<UserValues> = async (data: UserValues) => {
-        dispatch(setFormData({ ...data, apiKey }));
-        setIsFinishFillingForm(true);
-    };
+  const onSubmit: SubmitHandler<UserValues> = async (data: UserValues) => {
+    dispatch(setFormData({ ...data, apiKey }));
+    setIsFinishFillingForm(true);
+  };
 
-    return (
-        <>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="flex gap-7 flex-col items-center justify-center">
-                    <Typography
-                        variant="h5"
-                        className="mt-4 mb-4 font-poetsen text-dustyPaperDarkest"
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className='flex gap-7 flex-col items-center justify-center'>
+          <Typography
+            variant='h5'
+            className='mt-4 mb-4 font-poetsen text-dustyPaperDarkest'
+          >
+            Register
+          </Typography>
+
+          <Controller
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label='first name'
+                size='small'
+                helperText={errors[firstName] ? errors[firstName].message : ""}
+                error={errors[firstName] ? true : false}
+                sx={{ width: "200px" }}
+              />
+            )}
+            name={firstName}
+            control={control}
+          />
+
+          <Controller
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label='last name'
+                size='small'
+                helperText={errors[lastName] ? errors[lastName].message : ""}
+                error={errors[lastName] ? true : false}
+                sx={{ width: "200px" }}
+              />
+            )}
+            name={lastName}
+            control={control}
+          />
+
+          <Controller
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label={email}
+                onBlur={onBlur}
+                size='small'
+                helperText={errors[email] ? errors[email].message : ""}
+                error={errors[email] ? true : false}
+                sx={{ width: "200px" }}
+              />
+            )}
+            name={email}
+            control={control}
+          />
+
+          <Controller
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label={username}
+                onBlur={onBlur}
+                size='small'
+                helperText={errors[username] ? errors[username].message : ""}
+                error={errors[username] ? true : false}
+                sx={{ width: "200px" }}
+              />
+            )}
+            name={username}
+            control={control}
+          />
+
+          <Controller
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label={password}
+                size='small'
+                sx={{ width: "200px" }}
+                type={showPassword ? "text" : password}
+                helperText={errors[password] ? errors[password].message : ""}
+                error={errors[password] ? true : false}
+                InputProps={{
+                  endAdornment: (
+                    <IconButton
+                      onClick={() => {
+                        setShowPassword(!showPassword);
+                      }}
                     >
-                        Register
-                    </Typography>
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  ),
+                }}
+              />
+            )}
+            //@ts-ignore
+            name={password}
+            control={control}
+          />
 
-                    <Controller
-                        render={({ field }) => (
-                            <TextField
-                                {...field}
-                                label="first name"
-                                size="small"
-                                helperText={
-                                    errors[firstName]
-                                        ? errors[firstName].message
-                                        : ""
-                                }
-                                error={errors[firstName] ? true : false}
-                                sx={{ width: "200px" }}
-                            />
-                        )}
-                        name={firstName}
-                        control={control}
-                    />
+          <Controller
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label='confirm password'
+                type={showPassword ? "text" : password}
+                size='small'
+                helperText={
+                  errors[confirmPassword] ? errors[confirmPassword].message : ""
+                }
+                error={errors[confirmPassword] ? true : false}
+                sx={{ width: "200px" }}
+              />
+            )}
+            //@ts-ignore
+            name={confirmPassword}
+            control={control}
+          />
 
-                    <Controller
-                        render={({ field }) => (
-                            <TextField
-                                {...field}
-                                label="last name"
-                                size="small"
-                                helperText={
-                                    errors[lastName]
-                                        ? errors[lastName].message
-                                        : ""
-                                }
-                                error={errors[lastName] ? true : false}
-                                sx={{ width: "200px" }}
-                            />
-                        )}
-                        name={lastName}
-                        control={control}
-                    />
-
-                    <Controller
-                        render={({ field }) => (
-                            <TextField
-                                {...field}
-                                label={email}
-                                onBlur={onBlur}
-                                size="small"
-                                helperText={
-                                    errors[email] ? errors[email].message : ""
-                                }
-                                error={errors[email] ? true : false}
-                                sx={{ width: "200px" }}
-                            />
-                        )}
-                        name={email}
-                        control={control}
-                    />
-
-                    <Controller
-                        render={({ field }) => (
-                            <TextField
-                                {...field}
-                                label={username}
-                                onBlur={onBlur}
-                                size="small"
-                                helperText={
-                                    errors[username]
-                                        ? errors[username].message
-                                        : ""
-                                }
-                                error={errors[username] ? true : false}
-                                sx={{ width: "200px" }}
-                            />
-                        )}
-                        name={username}
-                        control={control}
-                    />
-
-                    <Controller
-                        render={({ field }) => (
-                            <TextField
-                                {...field}
-                                label={password}
-                                size="small"
-                                sx={{ width: "200px" }}
-                                type={showPassword ? "text" : password}
-                                helperText={
-                                    errors[password]
-                                        ? errors[password].message
-                                        : ""
-                                }
-                                error={errors[password] ? true : false}
-                                InputProps={{
-                                    endAdornment: (
-                                        <IconButton
-                                            onClick={() => {
-                                                setShowPassword(!showPassword);
-                                            }}
-                                        >
-                                            {showPassword ? (
-                                                <VisibilityOff />
-                                            ) : (
-                                                <Visibility />
-                                            )}
-                                        </IconButton>
-                                    ),
-                                }}
-                            />
-                        )}
-                        //@ts-ignore
-                        name={password}
-                        control={control}
-                    />
-
-                    <Controller
-                        render={({ field }) => (
-                            <TextField
-                                {...field}
-                                label="confirm password"
-                                type={showPassword ? "text" : password}
-                                size="small"
-                                helperText={
-                                    errors[confirmPassword]
-                                        ? errors[confirmPassword].message
-                                        : ""
-                                }
-                                error={errors[confirmPassword] ? true : false}
-                                sx={{ width: "200px" }}
-                            />
-                        )}
-                        //@ts-ignore
-                        name={confirmPassword}
-                        control={control}
-                    />
-
-                    <Button variant="contained" className="mb-4" type="submit">
-                        {"Let's get API key"}
-                    </Button>
-                </div>
-            </form>
-        </>
-    );
+          <Button variant='contained' className='mb-4' type='submit'>
+            {"Let's get API key"}
+          </Button>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default RegistrationForm;
--- frontend/src/app/drive/(components)/(layout)/(sideBar)/FolderTree.tsx
+++ frontend/src/app/drive/(components)/(layout)/(sideBar)/FolderTree.tsx
@@ -9,11 +9,11 @@ import { folder } from "@/constants";
 import { gotFolderChildren } from "@/redux/slices/utils";
 
 const Space = () => {
-  return <div className="w-[8px]" />;
+  return <div className='w-[8px]' />;
 };
 
 const TreeContainer: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
-  return <div className="flex flex-col text-left">{children}</div>;
+  return <div className='flex flex-col text-left'>{children}</div>;
 };
 
 type MyProps = {
@@ -38,12 +38,22 @@ export const TreeFragment: React.FC<MyProps> = ({ node, spaces }) => {
   const gotFolderChildren_ = gotFolderChildren(node);
   return (
     <TreeContainer>
-      <div className="flex cursor-pointer text-black  hover:bg-dustyPaperDark rounded-xl">
-        {spaces > 0 && new Array(spaces).fill(0).map((_, index) => <Space key={index} />)}
-        {node.isOpened && gotFolderChildren_ && <ArrowDropDownIcon onClick={() => handleArrowToggle(node)} />}
-        {!node.isOpened && gotFolderChildren_ && <ArrowRightIcon onClick={() => handleArrowToggle(node)} />}
-        {!gotFolderChildren_ && <div className="w-[24px]" />}
-        {<span className="text-left text-ellipsis flex-grow" onClick={() => onLabelClick(node)}>{`${node.name}`}</span>}
+      <div className='flex cursor-pointer text-black  hover:bg-dustyPaperDark rounded-xl'>
+        {spaces > 0 &&
+          new Array(spaces).fill(0).map((_, index) => <Space key={index} />)}
+        {node.isOpened && gotFolderChildren_ && (
+          <ArrowDropDownIcon onClick={() => handleArrowToggle(node)} />
+        )}
+        {!node.isOpened && gotFolderChildren_ && (
+          <ArrowRightIcon onClick={() => handleArrowToggle(node)} />
+        )}
+        {!gotFolderChildren_ && <div className='w-[24px]' />}
+        {
+          <span
+            className='text-left text-ellipsis flex-grow'
+            onClick={() => onLabelClick(node)}
+          >{`${node.name}`}</span>
+        }
       </div>
       {node.isOpened && node.children && (
         <>
@@ -53,7 +63,9 @@ export const TreeFragment: React.FC<MyProps> = ({ node, spaces }) => {
                 return node.type === folder;
               })
               .map((child: FSNode, index: number) => {
-                return <TreeFragment key={index} spaces={spaces + 1} node={child} />;
+                return (
+                  <TreeFragment key={index} spaces={spaces + 1} node={child} />
+                );
               })}
           </TreeContainer>
         </>
