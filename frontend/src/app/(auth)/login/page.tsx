2be3008f67482efcb484ef8f20fcb68ec37b1bdc
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-22 00:43:14 +0300

    remove unused code form login page

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -7,26 +7,18 @@ import Logo from "@/app/(common)/Logo";
 import Link from "next/link";
 
 const Login = () => {
-    const router = useRouter();
-    const authToken = useAppSelector((state) => state.general.authToken);
-    useEffect(() => {
-        console.log("login use effect authtoken: ", authToken);
-        if (authToken) {
-            router.push("/drive");
-        }
-    }, []);
-    return (
-        <div className="flex flex-col items-center">
-            <div className="mt-[20%]">
-                <Link href="/">
-                    <Logo />
-                </Link>
-            </div>
-            <div className="mt-[2.5%]">
-                <LoginForm />
-            </div>
-        </div>
-    );
+  return (
+    <div className='flex flex-col items-center'>
+      <div className='mt-[20%]'>
+        <Link href='/'>
+          <Logo />
+        </Link>
+      </div>
+      <div className='mt-[2.5%]'>
+        <LoginForm />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
