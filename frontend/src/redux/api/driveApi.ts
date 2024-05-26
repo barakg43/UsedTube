771649ca421343d1b20c23b29a01522fdabb718e
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-26 20:58:25 +0300

    add driveApi RTK toolkit

---
+++ frontend/src/redux/api/driveApi.ts
@@ -0,0 +1,20 @@
+import { FSNode, FileNode } from "@/types";
+import { baseApi } from "../baseApi";
+
+const driveApiSlice = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    folderContent: builder.query({
+      query: ({ folderId }: { folderId: string | undefined }) => ({
+        url: `/files/dir-content/${folderId ?? ""}`,
+        method: "GET",
+      }),
+      transformResponse: (
+        response: { data: { folders: FSNode[]; files: FileNode[] } },
+        meta,
+        arg
+      ) => response.data,
+    }),
+  }),
+});
+
+export const { useFolderContentQuery } = driveApiSlice;
