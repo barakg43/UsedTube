30ee52b7a86a3fc8da0ce6b20b2e6b8a7203d448
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-26 17:57:08 +0300

    create new AppUser modal that extend AbstractUser (User modal)

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,1 +1,10 @@
 # Register your models here.
+from django.contrib import admin
+
+from .models import AppUser
+@admin.register(AppUser)
+class AppUserAdmin(admin.ModelAdmin):
+
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_superuser', 'date_joined', 'storage_usage', 'root_folder')
+    search_fields = ('username','first_name', 'last_name', 'email')
+    list_filter = ('username', 'email')
--- backend/account/models.py
+++ backend/account/models.py
@@ -1,2 +1,47 @@
 # Create your models here.
 # class YouTubeAPIKey()
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, AbstractUser
+
+import files
+
+
+# class MyUserManager(BaseUserManager):
+#     def create_user(self, email, date_of_birth, password=None):
+#         """
+#         Creates and saves a User with the given email, date of
+#         birth and password.
+#         """
+#         if not email:
+#             raise ValueError("Users must have an email address")
+#
+#         user = self.model(
+#             email=self.normalize_email(email),
+#             date_of_birth=date_of_birth,
+#         )
+#
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+#
+#     def create_superuser(self, email, date_of_birth, password=None):
+#         """
+#         Creates and saves a superuser with the given email, date of
+#         birth and password.
+#         """
+#         user = self.create_user(
+#             email,
+#             password=password,
+#             date_of_birth=date_of_birth,
+#         )
+#         user.is_admin = True
+#         user.save(using=self._db)
+#         return user
+#
+
+class AppUser(AbstractUser):
+    storage_usage = models.IntegerField(default=0)
+    root_folder = models.ForeignKey("files.Folder", on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
