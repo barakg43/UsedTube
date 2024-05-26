b278529e121fe52a89929d70e60a054000220e60
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-26 20:59:26 +0300

    create temp {files, folders } object

--- frontend/src/app/drive/(components)/(layout)/(mainArea)/MainArea.tsx
+++ frontend/src/app/drive/(components)/(layout)/(mainArea)/MainArea.tsx
@@ -1,27 +1,40 @@
+import { useFolderContentQuery } from "@/redux/api/driveApi";
 import { useAppSelector } from "@/redux/hooks";
 import { RootState } from "@/redux/store";
 import { Typography } from "@mui/material";
-import React, { useState } from "react";
 import ItemsDisplay from "../../(itemsDisplay)/Display";
 import ItemsDisplayToggle from "../../(itemsDisplay)/ItemsDisplayToggle";
 import CreateNewFolder from "./CreateNewFolder";
+import { Suspense } from "react";
 
 const MainArea = ({ folderId }: { folderId: string | undefined }) => {
-    const activeDirectory = useAppSelector(
-        (state: RootState) => state.items.activeDirectory
-    );
-    return (
-        <div className="flex flex-col flex-grow px-4 py-4 mb-4 mr-4 bg-dustyPaper rounded-3xl">
-            <div className="flex flex-row justify-between w-full">
-                <Typography variant="h4">{activeDirectory?.name}</Typography>
-                <div className="flex flex-row justify-between w-[250px]">
-                    <CreateNewFolder />
-                    <ItemsDisplayToggle />
-                </div>
-            </div>
-            <ItemsDisplay />
+  console.log("folderId", folderId);
+  const { data, error, isLoading } = {
+    data: { folders: [], files: [] },
+    error: null,
+    isLoading: false,
+  };
+  //   useFolderContentQuery({ folderId });
+  console.log(data);
+  const { files, folders } = data || {};
+  const activeDirectory = useAppSelector(
+    (state: RootState) => state.items.activeDirectory
+  );
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div className='flex flex-col flex-grow px-4 py-4 mb-4 mr-4 bg-dustyPaper rounded-3xl'>
+      <div className='flex flex-row justify-between w-full'>
+        <Typography variant='h4'>{activeDirectory?.name}</Typography>
+        <div className='flex flex-row justify-between w-[250px]'>
+          <CreateNewFolder />
+          <ItemsDisplayToggle />
         </div>
-    );
+      </div>
+      <ItemsDisplay folders={folders || []} files={files || []} />
+    </div>
+  );
 };
 
 export default MainArea;
