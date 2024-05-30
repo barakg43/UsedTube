fd05197fded917ebacc0130fb2fb3c6fc2ac4973
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-30 09:16:26 +0300

    add icon for items in Display

--- frontend/src/app/drive/(components)/(itemsDisplay)/Display.tsx
+++ frontend/src/app/drive/(components)/(itemsDisplay)/Display.tsx
@@ -1,33 +1,48 @@
 import React from "react";
-import { file, grid } from "@/constants";
 import { FSNode, FileNode } from "@/types";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { setActiveDirectory } from "@/redux/slices/itemsSlice";
 import ItemsDisplayGrid from "./GridDisplay";
 import ItemsDisplayRow from "./RowDisplay";
+import { file, folder, grid } from "@/constants";
+import FolderIcon from "@mui/icons-material/Folder";
+import DescriptionIcon from "@mui/icons-material/Description";
+import { useRouter } from "next/navigation";
 
 type ItemsDisplayProp = {
   folders: FSNode[];
   files: FileNode[];
 };
 function ItemsDisplay({ files, folders }: ItemsDisplayProp) {
-  const items = useAppSelector((state) => state.items.activeDirectory.children);
+  const items = [...folders, ...files];
+  console.log("items:", items);
   const displayType = useAppSelector((state) => state.items.displayType);
   const dispatch = useAppDispatch();
+  const router = useRouter();
   const onEntryClick = (node: FSNode) => {
     // set active directory
     if (node.type === file) {
       return;
     }
     dispatch(setActiveDirectory(node));
+    router.push(`/drive/${node.id}`);
   };
 
+  const itemsWithIcon =
+    items?.map((item) =>
+      item.type === folder
+        ? { ...item, icon: <FolderIcon className='mr-5' /> }
+        : { ...item, icon: <DescriptionIcon className='mr-5' /> }
+    ) || [];
   return (
     <div className='bg-paper rounded-2xl px-4 py-4 flex flex-grow w-full h-full'>
       {displayType === grid ? (
         <ItemsDisplayGrid onEntryClick={onEntryClick} items={items || []} />
       ) : (
-        <ItemsDisplayRow onEntryClick={onEntryClick} items={items || []} />
+        <ItemsDisplayRow
+          onEntryClick={onEntryClick}
+          items={itemsWithIcon || []}
+        />
       )}
     </div>
   );
