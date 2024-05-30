1f0768b27fa77646ec5b2812db3a376d1c5a323d
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-30 09:15:56 +0300

    add icon for RowDisplay

--- frontend/src/app/drive/(components)/(itemsDisplay)/RowDisplay.tsx
+++ frontend/src/app/drive/(components)/(itemsDisplay)/RowDisplay.tsx
@@ -4,13 +4,14 @@ import { FC } from "react";
 
 const ItemsDisplayRow: FC<ItemsDisplayProp> = ({ items, onEntryClick }) => {
   const columns: GridColDef[] = [
+    { field: "icon", headerName: "", width: 30 },
     { field: "name", headerName: "Name", width: 200 },
     { field: "createdAt", headerName: "Created At", width: 200 },
     { field: "updatedAt", headerName: "Updated At", width: 200 },
   ];
 
   return (
-    <div className="h-[100%] w-[100%] relative">
+    <div className='h-[100%] w-[100%] relative'>
       <DataGrid
         columns={columns}
         rows={items}
