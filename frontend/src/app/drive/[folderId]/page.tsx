bc3d8b455f92bba47a9a18e49a3d44f7f5bb0288
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-06-02 18:47:30 +0300

    reuse new DriveComponent in drive pages

--- frontend/src/app/drive/[folderId]/page.tsx
+++ frontend/src/app/drive/[folderId]/page.tsx
@@ -1,27 +1,7 @@
-"use client";
-import { useRouter } from "next/navigation";
-import MainArea from "../(components)/(layout)/(mainArea)/MainArea";
-import Sidebar from "../(components)/(layout)/(sideBar)/Sidebar";
-import TopBar from "../(components)/(layout)/(topBar)/TopBar";
+import DriveComponent from "../DriveComponent";
 
 function Drive({ params }: { params?: { folderId: string | undefined } }) {
-  // const router = useRouter();
-  // const authToken = useAppSelector((state) => state.general.authToken);
-  // useEffect(() => {
-  //     if (!authToken) {
-  //         router.push("/login");
-  //     }
-  // }, []);
-  console.log(params?.folderId);
-  return (
-    <div className='flex flex-col h-full'>
-      <TopBar />
-      <div className='flex flex-row flex-grow bg-paper w-full h-full'>
-        <Sidebar />
-        <MainArea folderId={params?.folderId} />;
-      </div>
-    </div>
-  );
+  return <DriveComponent folderId={params?.folderId} />;
 }
 
 export default Drive;
