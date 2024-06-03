5940421d00e4c9185ecf9c7fdf9b09022c6ab1c3
omriSR <omri.1996s@gmail.com>
2024-06-03 19:22:01 +0300

    update sever endpoint when ready

--- frontend/src/app/drive/(components)/(layout)/(mainArea)/CreateNewFolder.tsx
+++ frontend/src/app/drive/(components)/(layout)/(mainArea)/CreateNewFolder.tsx
@@ -1,24 +1,40 @@
 import React, { useState } from "react";
 import CreateNewFolderIcon from "@mui/icons-material/CreateNewFolder";
+import axios from 'axios';
 import { Button, TextField, Typography, IconButton } from '@mui/material';
 import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "./theme";
+import { useAppSelector } from "@/redux/hooks";
+import { RootState } from "@/redux/store";
 
 function CreateNewFolder() {
   const [isInputVisible, setInputVisible] = useState(false);
   const [folderName, setFolderName] = useState('');
-
+  const activeDirectory = useAppSelector((state:RootState)=>  state.items.activeDirectory)
   const handleCreateFolder = () => {
     setInputVisible(true);
   };
 
-  const handleApprove = () => {
-    // TODO: Handle the logic for creating a new folder here
-    console.log(`Creating folder: ${folderName}`);
-    setInputVisible(false);
-    setFolderName('');
+  const handleApprove = async () => {
+    // TODO: update valid server endpoint when ready
+    try {
+      // Make the HTTP request to the server
+      const response = await axios.post('/api/create-folder', {
+        folderName,
+        activeDirectory,
+      });
+
+      // Handle the response as needed
+      console.log('Folder created:', response.data);
+
+      // Reset the state
+      setInputVisible(false);
+      setFolderName('');
+    } catch (error) {
+      console.error('Error creating folder:', error);
+    }
   };
 
   const handleCancel = () => {
