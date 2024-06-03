5193f48a91ef7388408a5c9ad42a1af0605d27db
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-06-03 07:26:21 +0300

    add NodeIconType and icon to drive items

--- frontend/src/app/drive/(components)/(itemsDisplay)/RowDisplay.tsx
+++ frontend/src/app/drive/(components)/(itemsDisplay)/RowDisplay.tsx
@@ -1,10 +1,22 @@
-import { ItemsDisplayProp } from "@/types";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { FSNode, ItemsDisplayProp, NodeType } from "@/types";
+import { DataGrid, GridColDef, GridColTypeDef } from "@mui/x-data-grid";
 import { FC } from "react";
+import ItemIcon from "./ItemIcon";
 
+const NodeIconType: GridColTypeDef<FSNode> = {
+  type: "custom",
+  resizable: false,
+  filterable: false,
+  sortable: false,
+  editable: false,
+  groupable: false,
+  display: "flex",
+  valueGetter: (value, row) => row,
+  renderCell: (params) => <ItemIcon type={params.value?.type} />,
+};
 const ItemsDisplayRow: FC<ItemsDisplayProp> = ({ items, onEntryClick }) => {
   const columns: GridColDef[] = [
-    { field: "icon", headerName: "", width: 30 },
+    { field: "icon", headerName: "", width: 30, ...NodeIconType },
     { field: "name", headerName: "Name", width: 200 },
     { field: "createdAt", headerName: "Created At", width: 200 },
     { field: "updatedAt", headerName: "Updated At", width: 200 },
