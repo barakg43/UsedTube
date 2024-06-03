f4ce4c52010a1057a5b702da99042112292f8907
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-06-03 07:33:00 +0300

    use new ItemIcon component in GridDisplayNode

--- frontend/src/app/drive/(components)/(itemsDisplay)/GridDisplayNode.tsx
+++ frontend/src/app/drive/(components)/(itemsDisplay)/GridDisplayNode.tsx
@@ -20,27 +20,34 @@ const ItemsDisplayNode: FC<{ node: FSNode; onEntryClick: Function }> = ({
   const handleMenuItemClick = (action: string) => {
     // Handle menu item click based on the selected action
     // For demonstration purposes, just log the action
-    console.log("Clicked:", action);
     handleClose();
   };
   return (
     <>
       <div
-        className="cursor-pointer flex-grow flex flex-row rounded-3xl px-2 py-2 mt-2 mr-6 bg-dustyPaper hover:bg-dustyPaperDark border"
-        onClick={(e) => (node.type == folder ? onEntryClick(node) : handleClick(e))}
+        className='cursor-pointer flex-grow flex flex-row rounded-3xl px-2 py-2 mt-2 mr-6 bg-dustyPaper hover:bg-dustyPaperDark border'
+        onClick={(e) =>
+          node.type == folder ? onEntryClick(node) : handleClick(e)
+        }
         onContextMenu={(e) => {
           e.preventDefault();
           handleClick(e);
         }}
       >
-        {node.type === folder ? <FolderIcon className="mr-5" /> : <DescriptionIcon className="mr-5" />}
-        <Typography className="text-ellipsis flex-grow">{node.name}</Typography>
-        {node.type === file && <MoreVertIcon className="hover:bg-dustyPaperEvenDarker rounded-full" />}
+        <ItemIcon type={node.type} />
+        <Typography className='text-ellipsis flex-grow'>{node.name}</Typography>
+        {node.type === file && (
+          <MoreVertIcon className='hover:bg-dustyPaperEvenDarker rounded-full' />
+        )}
       </div>
       <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
-        <MenuItem onClick={() => handleMenuItemClick("download")}>Download</MenuItem>
+        <MenuItem onClick={() => handleMenuItemClick("download")}>
+          Download
+        </MenuItem>
         <MenuItem onClick={() => handleMenuItemClick("share")}>Share</MenuItem>
-        <MenuItem onClick={() => handleMenuItemClick("delete")}>Delete</MenuItem>
+        <MenuItem onClick={() => handleMenuItemClick("delete")}>
+          Delete
+        </MenuItem>
       </Menu>
     </>
   );
