3397dba40cbe1417e99d1a253fa906f62a6613d5
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-06-03 07:29:58 +0300

    fix onClick not work on ItemsDisplayGrid,add type to folders&files array

--- frontend/src/app/drive/(components)/(itemsDisplay)/Display.tsx
+++ frontend/src/app/drive/(components)/(itemsDisplay)/Display.tsx
@@ -1,48 +1,44 @@
-import React from "react";
-import { FSNode, FileNode } from "@/types";
+import { grid } from "@/constants";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { setActiveDirectory } from "@/redux/slices/itemsSlice";
+import { FSNode, FileNode } from "@/types";
+import { useRouter } from "next/navigation";
 import ItemsDisplayGrid from "./GridDisplay";
 import ItemsDisplayRow from "./RowDisplay";
-import { file, folder, grid } from "@/constants";
-import FolderIcon from "@mui/icons-material/Folder";
-import DescriptionIcon from "@mui/icons-material/Description";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 type ItemsDisplayProp = {
   folders: FSNode[];
   files: FileNode[];
 };
 function ItemsDisplay({ files, folders }: ItemsDisplayProp) {
-  const items = [...folders, ...files];
-  console.log("items:", items);
+  const router = useRouter();
+  const filesWithType: FSNode[] = files.map((fileItem) => ({
+    ...fileItem,
+    type: "file",
+  }));
+  const folderWithType: FSNode[] = folders.map((folderItem) => ({
+    ...folderItem,
+    type: "folder",
+  }));
+  const items = [...folderWithType, ...filesWithType];
   const displayType = useAppSelector((state) => state.items.displayType);
   const dispatch = useAppDispatch();
-  const router = useRouter();
+  // const router = useRouter();
   const onEntryClick = (node: FSNode) => {
     // set active directory
-    if (node.type === file) {
-      return;
+    if (node.type === "folder") {
+      dispatch(setActiveDirectory(node));
+      router.push(`/drive/${node.id}`);
     }
-    dispatch(setActiveDirectory(node));
-    router.push(`/drive/${node.id}`);
   };
 
-  const itemsWithIcon =
-    items?.map((item) =>
-      item.type === folder
-        ? { ...item, icon: <FolderIcon className='mr-5' /> }
-        : { ...item, icon: <DescriptionIcon className='mr-5' /> }
-    ) || [];
   return (
     <div className='bg-paper rounded-2xl px-4 py-4 flex flex-grow w-full h-full'>
       {displayType === grid ? (
         <ItemsDisplayGrid onEntryClick={onEntryClick} items={items || []} />
       ) : (
-        <ItemsDisplayRow
-          onEntryClick={onEntryClick}
-          items={itemsWithIcon || []}
-        />
+        <ItemsDisplayRow onEntryClick={onEntryClick} items={items || []} />
       )}
     </div>
   );
