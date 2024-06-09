11f59f3de20173a7d907939ab588c3d76260ca19
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-06-09 07:44:18 +0300

    refactor whitespaces in Display

--- frontend/src/app/drive/(components)/(itemsDisplay)/Display.tsx
+++ frontend/src/app/drive/(components)/(itemsDisplay)/Display.tsx
@@ -8,9 +8,9 @@ import ItemsDisplayRow from "./RowDisplay";
 // import { useRouter } from "next/navigation";
 
 type ItemsDisplayProp = {
-    folders: FSNode[];
-    files: FileNode[];
-    parent: FSNode;
+  folders: FSNode[];
+  files: FileNode[];
+  parent: FSNode;
 };
 function ItemsDisplay({ files, folders, parent }: ItemsDisplayProp) {
   const router = useRouter();
@@ -38,34 +38,23 @@ function ItemsDisplay({ files, folders, parent }: ItemsDisplayProp) {
     }
   };
 
-    const displayType = useAppSelector((state) => state.items.displayType);
-    const dispatch = useAppDispatch();
-    // const router = useRouter();
-    const onEntryClick = (node: FSNode) => {
-        // set active directory
-        if (node.type === "folder") {
-            dispatch(setActiveDirectory(node));
-            router.push(`/drive/${node.id}`);
-        }
-    };
-
-    return (
-        <div className="bg-paper rounded-2xl px-4 py-4 flex flex-grow w-full h-full">
-            {displayType === GRID ? (
-                <ItemsDisplayGrid
-                    onEntryClick={onEntryClick}
-                    //@ts-ignore
-                    items={items || []}
-                />
-            ) : (
-                <ItemsDisplayRow
-                    onEntryClick={onEntryClick}
-                    //@ts-ignore
-                    items={items || []}
-                />
-            )}
-        </div>
-    );
+  return (
+    <div className='bg-paper rounded-2xl px-4 py-4 flex flex-grow w-full h-full'>
+      {displayType === GRID ? (
+        <ItemsDisplayGrid
+          onEntryClick={onEntryClick}
+          //@ts-ignore
+          items={items || []}
+        />
+      ) : (
+        <ItemsDisplayRow
+          onEntryClick={onEntryClick}
+          //@ts-ignore
+          items={items || []}
+        />
+      )}
+    </div>
+  );
 }
 
 export default ItemsDisplay;
