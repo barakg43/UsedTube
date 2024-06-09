6ca40d57d061cfb8bde1498ee199f0c2a92a15e2
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-06-09 07:39:07 +0300

    fix merging bug in driveApi

--- frontend/src/redux/api/driveApi.ts
+++ frontend/src/redux/api/driveApi.ts
@@ -1,54 +1,14 @@
 import { FSNode, FileNode } from "@/types";
 import { baseApi } from "../baseApi";
 import {
-    nextPhase,
-    setError,
-    setIsUploading,
-    setJobId,
-    setProgress,
+  nextPhase,
+  setError,
+  setIsUploading,
+  setJobId,
+  setProgress,
 } from "../slices/fileUploadSlice";
 
 const driveApiSlice = baseApi.injectEndpoints({
-        uploadFile: builder.mutation({
-            query: ({ file, folderId }: { file: File; folderId: string }) => {
-                const formData = new FormData();
-                formData.append("file", file);
-
-                return {
-                    url: `/files/upload/${folderId}`,
-                    method: "POST",
-                    body: formData,
-                };
-            },
-            transformResponse: (response: { job_id: string }) =>
-                response.job_id,
-            async onQueryStarted(
-                { file, folderId },
-                { dispatch, queryFulfilled }
-            ) {
-                try {
-                    const { data } = await queryFulfilled;
-                    //@ts-ignore
-                    dispatch(setJobId(data));
-                } catch (error) {
-                    dispatch(setError("Failed to upload file"));
-                    dispatch(setIsUploading(false));
-                }
-            },
-        }),
-        getUploadProgress: builder.query({
-            query: ({ jobId }: { jobId: string | null }) => ({
-                url: `/files/upload/progress/${jobId}`,
-                method: "GET",
-            }),
-            transformResponse: (response: { progress: number }) =>
-                response.progress * 100,
-            async onQueryStarted({ jobId }, { dispatch, queryFulfilled }) {
-                const { data } = await queryFulfilled;
-                dispatch(setProgress(data));
-                if (data === 100) dispatch(nextPhase());
-            },
-        }),
   endpoints: (builder) => ({
     folderContent: builder.query({
       query: ({ folderId }: { folderId: string | undefined }) =>
@@ -60,10 +20,48 @@ const driveApiSlice = baseApi.injectEndpoints({
         method: "GET",
       }),
     }),
+    uploadFile: builder.mutation({
+      query: ({ file, folderId }: { file: File; folderId: string }) => {
+        const formData = new FormData();
+        formData.append("file", file);
+
+        return {
+          url: `/files/upload/${folderId}`,
+          method: "POST",
+          body: formData,
+        };
+      },
+      transformResponse: (response: { job_id: string }) => response.job_id,
+      async onQueryStarted({ file, folderId }, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          //@ts-ignore
+          dispatch(setJobId(data));
+        } catch (error) {
+          dispatch(setError("Failed to upload file"));
+          dispatch(setIsUploading(false));
+        }
+      },
+    }),
+    getUploadProgress: builder.query({
+      query: ({ jobId }: { jobId: string | null }) => ({
+        url: `/files/upload/progress/${jobId}`,
+        method: "GET",
+      }),
+      transformResponse: (response: { progress: number }) =>
+        response.progress * 100,
+      async onQueryStarted({ jobId }, { dispatch, queryFulfilled }) {
+        const { data } = await queryFulfilled;
+        dispatch(setProgress(data));
+        if (data === 100) dispatch(nextPhase());
+      },
+    }),
+  }),
 });
 
 export const {
-    useFolderContentQuery,
-    useUploadFileMutation,
-    useGetUploadProgressQuery,
+  useFolderContentQuery,
+  useUploadFileMutation,
+  useDirectoryTreeQuery,
+  useGetUploadProgressQuery,
 } = driveApiSlice;
