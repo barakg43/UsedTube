aa15cf2401b3b74a58c83ca9b323cb7c3f63398d
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-06-09 07:41:08 +0300

    fix syntax error on SlideBar

--- frontend/src/app/drive/(components)/(layout)/(sideBar)/Sidebar.tsx
+++ frontend/src/app/drive/(components)/(layout)/(sideBar)/Sidebar.tsx
@@ -19,45 +19,44 @@ const SideBarItem: FC<{ children: ReactNode; hoverStyle?: boolean }> = ({
       {children}
     </div>
   );
+};
 const SideBarItem: FC<{ children: ReactNode; hoverStyle?: boolean }> = ({
-    children,
-    hoverStyle = true,
+  children,
+  hoverStyle = true,
 }) => {
-    return (
-        <div
-            className={`rounded-2xl bg-dustyPaper ${
-                hoverStyle
-                    ? " hover:bg-dustyPaperDark  cursor-pointer"
-                    : "cursor-default"
-            } items-center mb-3 text-black text-center`}
-        >
-            {children}
-        </div>
-    );
+  return (
+    <div
+      className={`rounded-2xl bg-dustyPaper ${
+        hoverStyle
+          ? " hover:bg-dustyPaperDark  cursor-pointer"
+          : "cursor-default"
+      } items-center mb-3 text-black text-center`}
+    >
+      {children}
+    </div>
+  );
 };
 
 const Sidebar = () => {
-    const tree = useAppSelector(
-        (state: RootState) => state.items.items.myItems
-    );
-    const file = useAppSelector((state: RootState) => state.fileUpload.file);
-    return (
-        <nav className="h-full w-[200px] flex flex-col px-2">
-            <SideBarItem>
-                <FileUpload />
-            </SideBarItem>
-            {file && (
-                <SideBarItem hoverStyle={false}>
-                    <SelectedFile />
-                </SideBarItem>
-            )}
-            <SideBarItem hoverStyle={false}>
-                <TreeFragment node={tree} spaces={0} />
-            </SideBarItem>
-            <SideBarItem>Shared with me</SideBarItem>
-            <SideBarItem>Quota and storage left</SideBarItem>
-        </nav>
-    );
+  const tree = useAppSelector((state: RootState) => state.items.items.myItems);
+  const file = useAppSelector((state: RootState) => state.fileUpload.file);
+  return (
+    <nav className='h-full w-[200px] flex flex-col px-2'>
+      <SideBarItem>
+        <FileUpload />
+      </SideBarItem>
+      {file && (
+        <SideBarItem hoverStyle={false}>
+          <SelectedFile />
+        </SideBarItem>
+      )}
+      <SideBarItem hoverStyle={false}>
+        <TreeFragment node={tree} spaces={0} />
+      </SideBarItem>
+      <SideBarItem>Shared with me</SideBarItem>
+      <SideBarItem>Quota and storage left</SideBarItem>
+    </nav>
+  );
 };
 
 export default Sidebar;
