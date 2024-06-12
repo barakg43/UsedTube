418acd1c0379e84e06fcb4a86f4aa532faa944f5
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-06-13 00:51:56 +0300

    comment navigate to login page in RequireAuth

--- frontend/src/app/(common)/RequireAuth.tsx
+++ frontend/src/app/(common)/RequireAuth.tsx
@@ -15,11 +15,11 @@ export default function RequireAuth({ children }: Props) {
   useVerifyToken();
   const { isLoading, isAuthenticated } = useAppSelector((state) => state.auth);
   const router = useRouter();
-  useEffect(() => {
-    if (!isAuthenticated) {
-      router.replace("/login");
-    }
-  }, [isAuthenticated, router]);
+  // useEffect(() => {
+  //   if (!isAuthenticated) {
+  //     router.replace("/login");
+  //   }
+  // }, [isAuthenticated, router]);
   if (isLoading) {
     return (
       <div className='flex justify-center my-8'>
