96eded3d017124cfa75cbcc57600f6ddb2dfc634
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-06-13 16:24:18 +0300

    change loading text to spinner FolderTree

--- frontend/src/app/drive/(components)/(layout)/(sideBar)/FolderTree.tsx
+++ frontend/src/app/drive/(components)/(layout)/(sideBar)/FolderTree.tsx
@@ -1,20 +1,19 @@
 import { useDirectoryTreeQuery } from "@/redux/api/driveApi";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-import { openAllAncestors, setItems } from "@/redux/slices/itemsSlice";
+import { setItems } from "@/redux/slices/itemsSlice";
 import { RootState } from "@/redux/store";
 import { useEffect } from "react";
 import TreeFragment from "./TreeFragment";
-import { FSNode } from "@/types";
+import { CircularProgress } from "@mui/material";
 
 function FolderTree() {
   const dispatch = useAppDispatch();
   const { data, isLoading } = useDirectoryTreeQuery(undefined);
-
   const { myItems } = useAppSelector((state: RootState) => state.items);
   useEffect(() => {
     dispatch(setItems(data));
   }, [data, dispatch]);
-  if (isLoading) return <p>loading</p>;
+  if (isLoading) return <CircularProgress />;
   return <TreeFragment node={myItems} spaces={0} />;
 }
 
