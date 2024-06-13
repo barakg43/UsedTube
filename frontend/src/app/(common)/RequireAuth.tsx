15aca0c64c72a91f6e14eafa215811d07b76cde8
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-06-13 16:25:05 +0300

    activate auth check and redirect to login page

--- frontend/src/app/(common)/RequireAuth.tsx
+++ frontend/src/app/(common)/RequireAuth.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import { redirect } from "next/navigation";
+import { useVerifyToken } from "@/hooks/auth/useVerifyToken";
 import { useAppSelector } from "@/redux/hooks";
 import { CircularProgress } from "@mui/material";
-import { useVerifyToken } from "@/hooks/auth/useVerifyToken";
-import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 interface Props {
   children: React.ReactNode;
@@ -15,11 +14,11 @@ export default function RequireAuth({ children }: Props) {
   useVerifyToken();
   const { isLoading, isAuthenticated } = useAppSelector((state) => state.auth);
   const router = useRouter();
-  // useEffect(() => {
-  //   if (!isAuthenticated) {
-  //     router.replace("/login");
-  //   }
-  // }, [isAuthenticated, router]);
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.replace("/login");
+    }
+  }, [isAuthenticated, router]);
   if (isLoading) {
     return (
       <div className='flex justify-center my-8'>
