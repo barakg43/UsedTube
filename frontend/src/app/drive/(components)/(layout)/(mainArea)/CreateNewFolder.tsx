0baaf28525a699799939abe0bb8a232ebe8726ba
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-07-05 18:18:43 +0300

    optimize import and spaces in front

--- frontend/src/app/drive/(components)/(layout)/(mainArea)/CreateNewFolder.tsx
+++ frontend/src/app/drive/(components)/(layout)/(mainArea)/CreateNewFolder.tsx
@@ -1,10 +1,9 @@
 "use client";
 import React from "react";
 import CreateNewFolderIcon from "@mui/icons-material/CreateNewFolder";
-import { useCreateFolderMutation } from "@/redux/api/driveApi";
-import { Button, TextField, Typography, IconButton } from '@mui/material';
-import CheckIcon from '@mui/icons-material/Check';
-import CloseIcon from '@mui/icons-material/Close';
+import { Button, TextField, Typography, IconButton } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
 import { useAppDispatch } from "@/redux/hooks";
 import { setAuth } from "@/redux/slices/authSlice";
 import { ThemeProvider } from "@emotion/react";
@@ -14,13 +13,15 @@ import { useToaster } from "@/app/(common)/useToaster";
 import { RootState } from "@/redux/store";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useCreateFolderMutation } from "@/redux/api/driveApi";
 
 function CreateNewFolder() {
   const [isInputVisible, setInputVisible] = useState(false);
-  const [folderName, setFolderName] = useState('');
+  const [folderName, setFolderName] = useState("");
   const dispatch = useAppDispatch();
-  const parentId = useAppSelector((state:RootState)=>  state.items.activeDirectoryId)
-  const [createFolder, { isLoading, error }] = useCreateFolderMutation();
+  const parentId = useAppSelector(
+    (state: RootState) => state.items.activeDirectoryId
+  );
   const router = useRouter();
   const handleCreateFolder = () => {
     setInputVisible(true);
@@ -28,44 +29,46 @@ function CreateNewFolder() {
 
   const handleApprove = async () => {
     try {
-      await createFolder({ folderName, parentId:'aeb13b3a-3552-4af9-ab5d-bceb01ef8d59' }).unwrap();
-      console.log(`Folder ${folderName} was created successfully`)
+      await createFolder({
+        folderName,
+        parentId: "aeb13b3a-3552-4af9-ab5d-bceb01ef8d59",
+      }).unwrap();
+      console.log(`Folder ${folderName} was created successfully`);
       // TODO: impliment front added folder
-    } catch{
-      console.error(`Failed to create folder ${folderName}`)
+    } catch {
+      console.error(`Failed to create folder ${folderName}`);
     }
   };
-  
-  
+
   const handleCancel = () => {
     setInputVisible(false);
-    setFolderName('');
+    setFolderName("");
   };
 
   return (
     <ThemeProvider theme={theme}>
-      <div className="flex items-center space-x-2 mx-8">
+      <div className='flex items-center space-x-2 mx-8'>
         {isInputVisible ? (
-          <div className="flex items-center space-x-2">
+          <div className='flex items-center space-x-2'>
             <TextField
-              size="small"
+              size='small'
               value={folderName}
               onChange={(e) => setFolderName(e.target.value)}
-              placeholder="New Folder Name"
+              placeholder='New Folder Name'
             />
-            <IconButton size="small" onClick={handleApprove}>
+            <IconButton size='small' onClick={handleApprove}>
               <CheckIcon />
             </IconButton>
-            <IconButton size="small" onClick={handleCancel}>
+            <IconButton size='small' onClick={handleCancel}>
               <CloseIcon />
             </IconButton>
           </div>
         ) : (
           <Button
-            className="hover:bg-dustyPaperDark text-black flex flex-row justify-left h-[47.33px]"
-            component="label"
-            variant="outlined"
-            size="small"
+            className='hover:bg-dustyPaperDark text-black flex flex-row justify-left h-[47.33px]'
+            component='label'
+            variant='outlined'
+            size='small'
             sx={{ textTransform: "none" }}
             onClick={handleCreateFolder}
           >
--- frontend/src/redux/api/driveApi.ts
+++ frontend/src/redux/api/driveApi.ts
@@ -2,84 +2,87 @@ import { baseApi } from "../baseApi";
 import { setError, setIsUploading, setJobId } from "../slices/fileUploadSlice";
 
 const driveApiSlice = baseApi.injectEndpoints({
-    endpoints: (builder) => ({
-        folderContent: builder.query({
-            query: ({ folderId }: { folderId: string | undefined }) =>
-                `/files/dir-content/${folderId || ""}`,
-        }),
-        directoryTree: builder.query({
-            query: () => ({
-                url: "/files/dir-tree/",
-                method: "GET",
-            }),
-        }),
-        uploadFile: builder.mutation({
-            query: ({ file, folderId }: { file: File; folderId: string }) => {
-                const formData = new FormData();
-                formData.append("file", file);
+  endpoints: (builder) => ({
+    folderContent: builder.query({
+      query: ({ folderId }: { folderId: string | undefined }) =>
+        `/files/dir-content/${folderId || ""}`,
+    }),
+    directoryTree: builder.query({
+      query: () => ({
+        url: "/files/dir-tree/",
+        method: "GET",
+      }),
+    }),
+    uploadFile: builder.mutation({
+      query: ({ file, folderId }: { file: File; folderId: string }) => {
+        const formData = new FormData();
+        formData.append("file", file);
 
-                return {
-                    url: `/files/upload/${folderId}`,
-                    method: "POST",
-                    body: formData,
-                };
-            },
-            transformResponse: (response: { job_id: string }) =>
-                response.job_id,
-            async onQueryStarted(
-                { file, folderId },
-                { dispatch, queryFulfilled }
-            ) {
-                try {
-                    const { data } = await queryFulfilled;
-                    //@ts-ignore
-                    dispatch(setJobId(data));
-                } catch (error) {
-                    dispatch(setError("Failed to upload file"));
-                    dispatch(setIsUploading(false));
-                }
-            },
-        }),
-        getSerializationProgress: builder.query({
-            query: ({ jobId }: { jobId: string | null }) => ({
-                url: `/files/upload/serialize/progress/${jobId}`,
-                method: "GET",
-            }),
-            transformResponse: (response: { progress: number }) => response,
-        }),
-        getUploadProgress: builder.query({
-            query: ({ jobId }: { jobId: string | null }) => ({
-                url: `/files/upload/progress/${jobId}`,
-                method: "GET",
-            }),
-            transformResponse: (response: { progress: number }) => response,
-        }),
+        return {
+          url: `/files/upload/${folderId}`,
+          method: "POST",
+          body: formData,
+        };
+      },
+      transformResponse: (response: { job_id: string }) => response.job_id,
+      async onQueryStarted({ file, folderId }, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          //@ts-ignore
+          dispatch(setJobId(data));
+        } catch (error) {
+          dispatch(setError("Failed to upload file"));
+          dispatch(setIsUploading(false));
+        }
+      },
+    }),
+    getSerializationProgress: builder.query({
+      query: ({ jobId }: { jobId: string | null }) => ({
+        url: `/files/upload/serialize/progress/${jobId}`,
+        method: "GET",
+      }),
+      transformResponse: (response: { progress: number }) => response,
     }),
     getUploadProgress: builder.query({
       query: ({ jobId }: { jobId: string | null }) => ({
         url: `/files/upload/progress/${jobId}`,
         method: "GET",
       }),
-      transformResponse: (
-        response: { data: { folders: FSNode[]; files: FileNode[] } },
-        meta,
-        arg
-      ) => response.data,
+      transformResponse: (response: { progress: number }) => response,
     }),
+
+    // getUploadProgress: builder.query({
+    //   query: ({ jobId }: { jobId: string | null }) => ({
+    //     url: `/files/upload/progress/${jobId}`,
+    //     method: "GET",
+    //   }),
+    //   transformResponse: (
+    //     response: { data: { folders: FSNode[]; files: FileNode[] } },
+    //     meta,
+    //     arg
+    //   ) => response.data,
+    // }),
     createFolder: builder.mutation({
-      query: ({ folderName, parentId }: { folderName: string; parentId: string }) => ({
+      query: ({
+        folderName,
+        parentId,
+      }: {
+        folderName: string;
+        parentId: string;
+      }) => ({
         url: `/files/create-folder`,
         method: "POST",
         body: { folderName, parentId },
-      })
+      }),
     }),
-  })
+  }),
 });
 
 export const {
-    useUploadFileMutation,
-    useFolderContentQuery,
-    useDirectoryTreeQuery,
-    useGetSerializationProgressQuery,
-    useGetUploadProgressQuery,
+  useUploadFileMutation,
+  useFolderContentQuery,
+  useDirectoryTreeQuery,
+  useGetSerializationProgressQuery,
+  useCreateFolderMutation,
+  useGetUploadProgressQuery,
 } = driveApiSlice;
