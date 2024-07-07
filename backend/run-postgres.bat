0c96f4ddadf57c653e18e37158d31850b202239a
Gal Aviezri <71984608+gaviezri@users.noreply.github.com>
2024-07-07 11:35:29 +0300

    Create run-postgres.bat

---
+++ backend/run-postgres.bat
@@ -0,0 +1,2 @@
+docker pull pazgas/usedtube:v1
+docker run --name postgres -d -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -v %cd%\Postgres\data:/var/lib/postgresql/data -p 5432:5432 pazgas/usedtube:v1
