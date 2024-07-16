8963a261e4f82afbd065ca7f24c50094313da8e0
Gal Aviezri <g.aviezri@gmail.com>
2024-07-16 21:09:27 +0300

    restructured file

--- frontend/src/app/drive/(components)/(itemsDisplay)/useHandleMenuItemClick.ts
+++ frontend/src/app/drive/(components)/(itemsDisplay)/useHandleMenuItemClick.ts
@@ -1,9 +1,29 @@
 import { useDeleteNodeMutation } from "@/redux/api/driveApi";
 import { ContextMenuAction, FSNode } from "@/types";
 
+// # 1
+
 export const useHandleMenuItemClick = () => {
     const [deleteNode] = useDeleteNodeMutation();
-    return (node: FSNode, action: ContextMenuAction) => {
+
+    const handleDelete = (nodeId: string) => {
+        deleteNode({
+            nodeId,
+        })
+            .unwrap()
+            .then((data) => {
+                console.log("Delete successful:", data);
+                alert("Delete successful :-) ## change to toaster! ##");
+            })
+            .catch((error) => {
+                console.error("Delete failed:", error);
+                alert("Delete unsuccessful >:-( ## change to toaster! ##");
+            });
+    };
+
+    const handleMenuItemClick = (node: FSNode, action: ContextMenuAction) => {
+        // # 3
+
         switch (action) {
             case "download":
                 console.log("Downloading:", node);
@@ -12,22 +32,10 @@ export const useHandleMenuItemClick = () => {
                 console.log("Sharing:", node);
                 break;
             case "delete":
-                deleteNode({
-                    nodeId: node.id,
-                })
-                    .unwrap()
-                    .then((data) => {
-                        console.log("Delete successful:", data);
-                        alert("Delete successful :-) ## change to toaster! ##");
-                    })
-                    .catch((error) => {
-                        console.error("Delete failed:", error);
-                        alert(
-                            "Delete unsuccessful >:-( ## change to toaster! ##"
-                        );
-                    });
-
+                handleDelete(node.id);
                 break;
         }
     };
+
+    return handleMenuItemClick;
 };
