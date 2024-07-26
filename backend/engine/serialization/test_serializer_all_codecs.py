49cf127da422250b48da5925e94dba726d596e9e
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-07-26 09:13:53 +0300

    make changes in test_serializer to support new Driver class

--- backend/engine/serialization/test_serializer_all_codecs.py
+++ backend/engine/serialization/test_serializer_all_codecs.py
@@ -26,21 +26,16 @@ codec_arry = [
 for codec_pair in codec_arry:
     codec = codec_pair[0]
     file_ext = codec_pair[1]
-    test_method_name_3P_2B = 'test_serializer_pdf_3B_2P_{0}_{1}'.format(codec, file_ext)
     test_method_name_Bit_Block = 'test_serializer_pdf_1B_1Block_{0}_{1}'.format(codec, file_ext)
 
 
     # Function to perform the test for a specific codec
-    def test_3p_2B(self, codec_arg=codec, file_ext_arg=file_ext):
-        SerializerTest.perform_test_3P_2B(self, codec_arg, file_ext_arg)
-
 
     def test_bit_block(self, codec_arg=codec, file_ext_arg=file_ext):
         SerializerTest.perform_test_1Bit_Block(self, codec_arg, file_ext_arg)
 
 
-    # setattr(SerializerTest, test_method_name_3P_2B, test_3p_2B)
     setattr(SerializerTest, test_method_name_Bit_Block, test_bit_block)
-    # break
+    break
 if __name__ == '__main__':
     unittest.main()
