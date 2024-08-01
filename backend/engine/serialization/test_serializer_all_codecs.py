58cff1e3649f62daff5dac186d3b8db485eaf020
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-08-01 12:55:37 +0300

    make new codec_list in comment

--- backend/engine/serialization/test_serializer_all_codecs.py
+++ backend/engine/serialization/test_serializer_all_codecs.py
@@ -2,89 +2,46 @@ import unittest
 
 from engine.serialization.test_serializer import SerializerTest
 
-# codec_arry = [
-#
-#     # ['RGBA', 'avi'],  # too big
-#     # ['av01', 'mp4'], not working
-#     # ['vp09', 'mp4'],
-#
-#     # ['mp4v', 'mp4'],
-#     # ['avc1', 'mp4'],
-#     # ['avc3', 'mp4'],
-#
-#     # ['drac', 'mp4'], # not working
-#     ['hev1', 'mp4'],# not working
-#     ['hvc1', 'mp4'], # not working
-#     # ['mhm1', 'mp4'], # not working
-#     # ['mlpa', 'mp4'], # not working
-#     # ['mp4s', 'mp4'], # not working
-#     ['mp4v', 'mp4'],
-#     # ['vc-1', 'mp4'],# not working
-#     ['davc', 'mp4'],
-#     ['xvid', 'mp4']
-# ]
 codec_list = [
-    # 'aac',
-    # 'aac_fixed',
-    # 'ac3',
-    # 'ac3_fixed',
-    # 'alac',
-    # 'als',
-    # 'av1',
-    # 'dca',
-    # 'dirac',
-    # 'dvdsub',
-    # 'eac3',
-    # 'evrc',
-    # 'flac',
-    'h264',
-    # 'h264_mf',
-    'hevc',
-    # 'hevc_mf',
-    'jpeg2000',
-    'libaom-av1',
-    'libdav1d',
-    # 'libmp3lame',
-    # 'libopenjpeg',
-    'libopus',
-    # 'libtwolame',
-    'libvorbis',
-    'libvpx-vp9',
-    # 'libx264',
-    # 'libx264rgb',
-    # 'libx265',
-    'mjpeg',
-    'mov_text',
-    'mp2',
-    # 'mp2fixed',
-    'mp2float',
-    'mp3',
-    # 'mp3_mf',
-    'mp3float',
-    'mpeg1video',
-    'mpeg2video',
-    'mpeg4',
-    'mpegvideo',
-    'opus',
-    'pcm_f32be',
-    'pcm_f32le',
-    'pcm_f64be',
-    'pcm_f64le',
-    'pcm_s16be',
-    'pcm_s16le',
-    'pcm_s24be',
-    'pcm_s24le',
-    'pcm_s32be',
-    'pcm_s32le',
-    'png',
-    'qcelp',
-    'truehd',
-    'tscc2',
-    # 'ttml',
-    'vc1',
-    # 'vc2',
-    'vorbis',
-    'vp9']
+
+    # ['RGBA', 'avi'],  # too big
+    # ['av01', 'mp4'], not working
+    # ['vp09', 'mp4'],
+
+    'mp4v',
+    'avc1',
+    'avc3',
+
+    # ['drac', 'mp4'], # not working
+    # 'hev1',# not working
+    # 'hvc1', # not working
+    # ['mhm1', 'mp4'], # not working
+    # ['mlpa', 'mp4'], # not working
+    # ['mp4s', 'mp4'], # not working
+    'mp4v',
+    # ['vc-1', 'mp4'],# not working
+   'davc',
+    'xvid',
+]
+# codec_list = [
+# # 'dirac',
+# 'mpegvideo',
+# 'libvpx-vp9',
+# 'vc1',
+# 'jpeg2000',
+# 'h264',
+# 'vp9',
+# 'tscc2',
+# 'mpeg1video',
+# 'mpeg2video',
+# 'hevc',
+# 'png',
+# 'libdav1d',
+# 'mjpeg',
+# 'mpeg4',
+# 'vp9'
+#     ]
+
 
 # Test class for running tests for each codec
 for codec_pair in codec_list:
