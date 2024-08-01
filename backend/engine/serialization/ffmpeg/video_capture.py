ca4542860e1ac24d6473194a8e5b8977cd4e8b0c
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-08-01 12:52:35 +0300

    fix VideoCapture bugs in reading frames

--- backend/engine/serialization/ffmpeg/video_capture.py
+++ backend/engine/serialization/ffmpeg/video_capture.py
@@ -1,43 +1,74 @@
+import av
 import numpy as np
+from av.video.stream import VideoStream
 
 
 class VideoCapture:
-    def __init__(self, video_stream_path, logging_stdout=False):
+    def __init__(self, video_stream_path):
+        self.error_massage = None
+        self.video_input = None
+        try:
+            self.video_input = av.open(video_stream_path)
+        except Exception as e:
+            self.error_massage = str(e)
+            return
         self.path = video_stream_path
-        self.video_stream = (
-            ffmpeg.input(video_stream_path)
-            .output('pipe:', format='rawvideo', pix_fmt='bgr24')
-            .run_async(pipe_stdout=True, quiet=not logging_stdout)
-        )
+        self.frame_counter=0
+        self.video_stream: VideoStream = self.video_input.streams.video[0]
+        self.frames_generator = self.video_input.decode(video=0)
+        self.video_props = self.__get_video_props()
+        # self.frame_size_bytes = self.video_props["width"] * self.video_props[
+        #     "height"] * 3  # H * W * 3 channels * 1-byte/channel
 
-        self.video_props = self.__get_video_props(video_stream_path)
-        self.frame_size_bytes = self.video_props["width"] * self.video_props[
-            "height"] * 3  # H * W * 3 channels * 1-byte/channel
-
-    def __get_video_props(self, video_stream_path):
-        probe = ffmpeg.probe(video_stream_path)
-        video_stream_details = next(
-            (stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
-        width = int(video_stream_details['width'])
-        height = int(video_stream_details['height'])
-        probe = ffmpeg.probe(video_stream_path)
-        video_stream_details = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
-        fps = int(video_stream_details['r_frame_rate'].split('/')[0])
-        codec = video_stream_details['codec_name']
+    def __get_video_props(self):
+        self.__check_if_open()
+        # probe = ffmpeg.probe(video_stream_path)
+        # video_stream_details = next(
+        #     (stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+        width = self.video_stream.width
+        height = self.video_stream.height
+        # video_stream_details = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+        fps = int(self.video_stream.average_rate)
+        codec = self.video_stream.codec_context.name
         return {"width": width, "height": height, "fps": fps, "codec": codec}
 
     def read(self):
-        buffer = self.video_stream.stdout.read(self.frame_size_bytes)
-        #  check buffer length is not W*H*3 (when FFmpeg streaming ends).
-        ret = len(buffer) == self.frame_size_bytes
-        width, height = self.video_props["width"], self.video_props[
-            "height"]
-        frame = np.frombuffer(buffer, np.uint8).reshape(height, width, 3)
-        return ret, frame
+        self.__check_if_open()
+        frame_cv = next(self.frames_generator, None)
+        isValid: bool = frame_cv is not None
+        if not isValid and self.frame_counter==0:
+            raise BrokenPipeError("Empty frame stream")
+        if isValid:
+            frame = frame_cv.to_ndarray(format="bgr24")
+            self.frame_counter += 1
+            return isValid, frame
+        else:
+            return isValid, None
 
+    # buffer = self.video_stream.stdout.read(self.frame_size_bytes)
+    # #  check buffer length is not W*H*3 (when FFmpeg streaming ends).
+    # ret = len(buffer) == self.frame_size_bytes
+    # width, height = self.video_props["width"], self.video_props[
+    #     "height"]
+    # frame = np.frombuffer(buffer, np.uint8).reshape(height, width, 3)
     def get_video_props(self):
+        self.__check_if_open()
+
         return self.video_props
 
+    def set(self, offset: int):
+        self.__check_if_open()
+
+        self.video_input.seek(offset)
+
+    def isOpened(self):
+        return self.error_massage is None
+
+    def __check_if_open(self):
+        if not self.isOpened():
+            raise FileNotFoundError(self.error_massage)
+
     def release(self):
-        self.video_stream.stdout.close()
-        self.video_stream.wait()
+        self.__check_if_open()
+
+        self.video_input.close()
