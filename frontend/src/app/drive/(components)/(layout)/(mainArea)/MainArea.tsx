b2671136636daffbcda7d342c38cfa48f0424f55
Gal Aviezri <g.aviezri@gmail.com>
2024-08-04 14:06:24 +0300

    set root dir id in redux on initial load

--- frontend/src/app/drive/(components)/(layout)/(mainArea)/MainArea.tsx
+++ frontend/src/app/drive/(components)/(layout)/(mainArea)/MainArea.tsx
@@ -1,34 +1,21 @@
 "use client";
-import { useToaster } from "@/app/(common)/(hooks)/(toaster)/useToaster";
 import { useFolderContentQuery } from "@/redux/api/driveApi";
-import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-import { RootState } from "@/redux/store";
 import { Typography } from "@mui/material";
 import ItemsDisplay from "../../(itemsDisplay)/Display";
 import ItemsDisplayToggle from "../../(itemsDisplay)/ItemsDisplayToggle";
 import CreateNewFolder from "./CreateNewFolder";
 import Loading from "@/app/(common)/(components)/Loading";
-import { useEffect } from "react";
-import { setActiveDirectory } from "@/redux/slices/itemsSlice";
 import ParentsRow from "./ParentsRow";
 import { FSNode, FileNode } from "@/types";
 
 const MainArea = ({ folderId }: { folderId: string | undefined }) => {
     const { data, error, isLoading } = useFolderContentQuery({ folderId });
-    const dispatch = useAppDispatch();
     const {
         files,
         folders,
         parents,
     }: { files: FileNode[]; folders: FSNode[]; parents: FSNode[] } = data || {};
-    const activeDirectory = useAppSelector(
-        (state: RootState) => state.items.activeDirectoryId
-    );
-    useEffect(() => {
-        // if (!activeDirectory)
-        // dispatch(setActiveDirectory(parents && parents[0]?.id));
-        dispatch(setActiveDirectory(folderId || ""));
-    });
+
     if (isLoading) {
         <Loading />;
     }
