b6385eedad4cbf8c895bff9dc001f3f7ddf23bfb
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-08-04 10:56:16 +0300

    make obfuscate return video with minimum length to support smaller file

--- backend/engine/obfuscation/obfuscation_manager.py
+++ backend/engine/obfuscation/obfuscation_manager.py
@@ -3,7 +3,7 @@ import uuid
 
 import cv2
 
-from engine.constants import FILES_READY_FOR_STORAGE_DIR, GENERAL_LOGGER, TMP_WORK_DIR
+from engine.constants import FILES_READY_FOR_STORAGE_DIR, GENERAL_LOGGER, TMP_WORK_DIR, MINIMUM_VIDEO_FRAME_AMOUNT
 
 
 class ObfuscationManager:
@@ -47,31 +47,34 @@ class ObfuscationManager:
         out_path = (FILES_READY_FOR_STORAGE_DIR / f"{uuid.uuid4()}.mp4").as_posix()
         fourcc = cv2.VideoWriter.fourcc(*fourcc)
         out = cv2.VideoWriter(out_path, fourcc, fps, (width, height))
-        counter = 0
-        while True and counter < 1800:
+        frame_counter = 0
+        while True:
             ret_ff, frame_ff = file_frames_video.read()
             if not ret_ff:
                 self.logger.info("loop ended. No more frames in file_frames")
                 break
 
             out.write(frame_ff)
-            counter += 1
-
-            for _ in range(self.cycle):
-                ret_cov, frame_cov = cover_video.read()
-                if not ret_cov:
-                    self.logger.info("No more frames in cover_video")
-                    break
-                counter += 1
-                out.write(frame_cov)
-
+            frame_counter += 1
+            frame_counter += self.__write_frames_from_obfuscated_to_output_video(out, cover_video, self.cycle)
+        if frame_counter < MINIMUM_VIDEO_FRAME_AMOUNT: #make the output video at least 'MINIMUM_VIDEO_FRAME_AMOUNT' frames
+            self.__write_frames_from_obfuscated_to_output_video( out, cover_video, MINIMUM_VIDEO_FRAME_AMOUNT - frame_counter)
                 # Release video capture and writer
         file_frames_video.release()
         cover_video.release()
         out.release()
 
         return out_path
-
+    def __write_frames_from_obfuscated_to_output_video(self, obfuscated_video_out: cv2.VideoWriter, cover_video:  cv2.VideoCapture,frame_amount:int):
+        frame_write_counter = 0
+        for _ in range(frame_amount):
+            ret_cov, frame_cov = cover_video.read()
+            if not ret_cov:
+                self.logger.info("No more frames in cover_video")
+                break
+            obfuscated_video_out.write(frame_cov)
+            frame_write_counter+=1
+        return frame_write_counter
     def untangle(self, obfuscated_video_path: str) -> str:
         # open the obfuscated_video as obsv
         # create a new out video container as out
@@ -93,14 +96,12 @@ class ObfuscationManager:
         out_path = (TMP_WORK_DIR / f"{uuid.uuid4()}.mp4").as_posix()  # Generate unique filename using UUID
         fourcc = cv2.VideoWriter.fourcc(*'mp4v')
         out = cv2.VideoWriter(out_path, fourcc, fps, (width, height))
-
         while True:
             ret, frame = obsv.read()
             if not ret:
                 break
 
             out.write(frame)
-
             # Skip {cycle} frames
             for _ in range(self.cycle):
                 _ = obsv.read()
