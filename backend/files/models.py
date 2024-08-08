c77f96a1236227afadc3be178b15293a8da2e34d
Gal Aviezri <g.aviezri@gmail.com>
2024-08-08 12:36:14 +0300

    create new django app sharing

--- backend/files/models.py
+++ backend/files/models.py
@@ -37,19 +37,3 @@ class File(models.Model):
 
     def __str__(self):
         return f"{self.name}.{self.extension}"
-
-
-class SharedItem(models.Model):
-    folder_item = models.ForeignKey(
-        AppUser, on_delete=models.CASCADE, null=True, related_name="shared_folders"
-    )
-    file_item = models.ForeignKey(
-        File, on_delete=models.CASCADE, null=True, related_name="shared_files"
-    )
-    shared_with = models.ForeignKey(
-        AppUser, on_delete=models.CASCADE, related_name="shared_items"
-    )
-    shared_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = ("folder_item", "file_item", "shared_with")
---
+++ backend/sharing/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from sharing.models import SharedItem
+
+# Register your models here.
+@admin.register(SharedItem)
+class SharedItemAdmin(admin.ModelAdmin):
+    list_display = ('folder_item', 'file_item', 'shared_with', 'shared_at')
+    search_fields = ('item__name', 'shared_with__username')
+    list_filter = ('shared_with',)
+    raw_id_fields = ('folder_item', 'file_item', 'shared_with')
