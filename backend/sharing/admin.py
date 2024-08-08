5cebd5ddc36ec8cfee0e0d48161ee856e37eabb6
Gal Aviezri <g.aviezri@gmail.com>
2024-08-08 14:40:50 +0300

    changed SharedItemAdmin

--- backend/sharing/admin.py
+++ backend/sharing/admin.py
@@ -5,7 +5,7 @@ from sharing.models import SharedItem
 # Register your models here.
 @admin.register(SharedItem)
 class SharedItemAdmin(admin.ModelAdmin):
-    list_display = ('folder_item', 'file_item', 'shared_with', 'shared_at')
+    list_display = ('file_item', 'shared_with', 'shared_at')
     search_fields = ('item__name', 'shared_with__username')
     list_filter = ('shared_with',)
-    raw_id_fields = ('folder_item', 'file_item', 'shared_with')
+    raw_id_fields = ('file_item', 'shared_with')
