30e9096944aa0ebcc32617c08289556d0139aad7
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-08-18 18:20:40 +0300

    small code spaces refactor

--- backend/engine/serialization/test_serializer.py
+++ backend/engine/serialization/test_serializer.py
@@ -6,7 +6,7 @@ import unittest
 import numpy as np
 
 from engine.constants import TEST_RESOURCES_DIR, TEST_OUTPUT_DIR, SAMPLE_FILE_3, COVER_VIDEOS_DIR, \
-    FILES_READY_FOR_STORAGE_DIR, SAMPLE_FILE_1
+    FILES_READY_FOR_STORAGE_DIR, SAMPLE_FILE_1, SAMPLE_FILE_2
 from engine.driver import Driver
 from engine.serialization.serializer import Serializer
 from engine.serialization.stateless_serializer import StatelessSerializer
@@ -20,15 +20,17 @@ ENC_OUT_VID_PATH = 1
 COVER_VID_PATH = 2
 DEC_PDF_PATH = 3
 
-csv_path="C:\\ComputerScience\\Workshop\\UsedTube\\backend\\engine\\artifacts\\awaiting_storage\\out_1.csv"
-file=open(csv_path, 'w', newline='')
+csv_path = "C:\\ComputerScience\\Workshop\\UsedTube\\backend\\engine\\artifacts\\awaiting_storage\\out_1.csv"
+file = open(csv_path, 'w', newline='')
 csv_file = csv.writer(file, dialect='excel', delimiter=',')
-csv_file.writerow(["codec", "encode_time", "decode_time","video_size"])
+csv_file.writerow(["codec", "encode_time", "decode_time", "video_size"])
+
+
 class SerializerTest(unittest.TestCase):
 
     def paths_dict(self):
         original_file_ext = "pdf"
-        paths_dict = {PDF_PATH: (TEST_RESOURCES_DIR / SAMPLE_FILE_1).as_posix(),
+        paths_dict = {PDF_PATH: (TEST_RESOURCES_DIR / SAMPLE_FILE_2).as_posix(),
                       ENC_OUT_VID_PATH: (
                               TEST_OUTPUT_DIR / "output-video_{fourcc}.{out_format}")
                       .as_posix(),
@@ -38,33 +40,33 @@ class SerializerTest(unittest.TestCase):
 
         return paths_dict
 
-    def check_pdf_serialization(self, fourcc:str,out_format:str,block_size:int):
+    def check_pdf_serialization(self, fourcc: str, out_format: str, block_size: int):
         # proto = StatelessSerializerArgs(fourcc,out_format,block_size)
 
-        driver = DriverArgs(fourcc,out_format,block_size)
+        driver = DriverArgs(fourcc, out_format, block_size)
         # self.enc = StatelessSerializer(proto, concurrent_execution=True)
         paths = self.paths_dict()
-        video_encoded_path=paths[ENC_OUT_VID_PATH].format(fourcc=fourcc,out_format=out_format)
+        video_encoded_path = paths[ENC_OUT_VID_PATH].format(fourcc=fourcc, out_format=out_format)
         begin_time = time.time()
-        obfuscated_vid_path, zipped_file_size=driver.process_file_to_video(paths[PDF_PATH], "1")
+        obfuscated_vid_path, zipped_file_size = driver.process_file_to_video(paths[PDF_PATH], "1")
         # obfuscated_vid_path, zipped_file_size=(
         #    (FILES_READY_FOR_STORAGE_DIR/"39d6440b-d37c-43bc-ab85-7bebfaed5307.mp4").as_posix(),
         #     12233262
         # )
-        print( obfuscated_vid_path, zipped_file_size)
+        print(obfuscated_vid_path, zipped_file_size)
         # self.enc.serialize(paths[DEC_PDF_PATH], paths[COVER_VID_PATH], paths[ENC_OUT_VID_PATH])
         end_time = time.time()
-        encode_time=end_time - begin_time
+        encode_time = end_time - begin_time
         print(f"Encoded In {encode_time}")
         file_size = os.stat(paths[PDF_PATH]).st_size
         begin_time = time.time()
-        deserialized_pdf_path = driver.process_video_to_file(obfuscated_vid_path, zipped_file_size,"1",fourcc)
+        deserialized_pdf_path = driver.process_video_to_file(obfuscated_vid_path, zipped_file_size, "1", fourcc)
         # self.enc.deserialize(serialized_file_as_video_path=paths[ENC_OUT_VID_PATH],file_size= file_size, deserialized_pdf_file=paths[DEC_PDF_PATH])
         end_time = time.time()
-        decode_time=end_time - begin_time
+        decode_time = end_time - begin_time
         print(f"Decoded In {decode_time}")
         print(deserialized_pdf_path)
-        csv_file.writerow([fourcc, encode_time, decode_time,os.stat(obfuscated_vid_path).st_size])
+        csv_file.writerow([fourcc, encode_time, decode_time, os.stat(obfuscated_vid_path).st_size])
         deserialized_pdf_file = open(deserialized_pdf_path, "rb")
         decrypted_sha256 = StatelessSerializerArgs.generateSha256ForFile(deserialized_pdf_file)
         pdf_file = open(paths[PDF_PATH], 'rb')
@@ -117,13 +119,13 @@ class SerializerTest(unittest.TestCase):
 
         return original_sha256, deserialized_sha256
 
-    def perform_test_1Bit_Block(self, codec, file_ext,block_size=4):
+    def perform_test_1Bit_Block(self, codec, file_ext, block_size=4):
         # Replace this with your actual test implementation
         print(f"#### Bit to Block: Testing codec '{codec}' with file extension '.{file_ext}' ###")
         original_sha256, decrypted_sha256 = self.check_pdf_serialization(fourcc=codec,
                                                                          out_format=file_ext,
                                                                          block_size=block_size)
-        self.assertEqual( original_sha256, decrypted_sha256)
+        self.assertEqual(original_sha256, decrypted_sha256)
 
     def perform_test_youtube_1B_1Block_decryption(self, codec, file_ext, encrypted_video_path, original_file_path):
         # Replace this with your actual test implementation
