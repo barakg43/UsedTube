4c76652410556f8225155628662b28435c0a569d
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-08-21 06:57:48 +0300

    testing bitrate and block size permutation in all codec test

--- backend/engine/serialization/test_serializer_all_codecs.py
+++ backend/engine/serialization/test_serializer_all_codecs.py
@@ -1,6 +1,6 @@
 import unittest
 
-from engine.serialization.test_serializer import SerializerTest
+from engine.serialization.test_serializer import SerializerTest, csv_file
 
 codec_list = [
     # "av1",
@@ -41,15 +41,21 @@ codec_list = [
 for codec_pair in codec_list:
     codec = codec_pair
     file_ext = "mp4"
-    for block_size in range(4, 20,2):
-        test_method_name_Bit_Block = 'test_serializer_pdf_1B_1Block_{0}_{1}_Block:{2}'.format(codec, file_ext, block_size)
-        # Function to perform the test for a specific codec
-        def test_bit_block(self, codec_arg=codec, file_ext_arg=file_ext, block_size_arg=20):
-            SerializerTest.perform_test_1Bit_Block(self, codec_arg, file_ext_arg, block_size_arg)
-        setattr(SerializerTest, test_method_name_Bit_Block, test_bit_block)
+
+    for block_size in range(10, 30, 2):
+        for bitrate in range(1000, 20001, 1000):
+            test_method_name_Bit_Block = 'test_serializer_pdf_1B_1Block_{0}_{1}_Block_{2:02}_bitrate_{3:05}'.format(
+                codec, file_ext, block_size, bitrate)
 
 
+            # Function to perform the test for a specific codec
+            def test_bit_block(self, codec=codec, file_ext=file_ext, block_size=block_size, bitrate=bitrate):
+                SerializerTest.perform_test_1Bit_Block(self, codec, file_ext, block_size, bitrate)
+
+
+            setattr(SerializerTest, test_method_name_Bit_Block, test_bit_block)
 
     # break
 if __name__ == '__main__':
     unittest.main()
+    csv_file.close()
