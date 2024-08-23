a4d303bc6f54e2575ab9ef2c0b9919610427d386
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-08-23 17:52:17 +0300

    use single Driver instance instead of new for each operation

--- backend/engine/manager.py
+++ backend/engine/manager.py
@@ -1,5 +1,4 @@
 import logging
-import os
 from concurrent.futures import ThreadPoolExecutor, Future
 from threading import Lock
 from typing import Dict, Tuple, Callable
@@ -11,7 +10,6 @@ from engine.downloader.definition import Downloader
 from engine.driver import Driver
 from engine.progress_tracker import Tracker
 from engine.uploader.Dailymotion.uploader import DailymotionUploader, Mr_DailymotionUploader
-from files.progress_tracker import ProgressTracker
 
 
 class EngineManager:
@@ -33,6 +31,7 @@ class EngineManager:
             return
         self.initialized = True
         self.workers = ThreadPoolExecutor(10)
+        self.driver = Driver()
         self.uuid_to_future: Dict[uuid1, Future] = {}
         self.uuid_to_progress: Dict[uuid1, float] = {}
 
@@ -41,12 +40,12 @@ class EngineManager:
 
     def process_file_to_video_async(self, file_path: str) -> uuid1:
         uuid = str(uuid1())
-        self.uuid_to_future[uuid] = self.workers.submit(Driver().process_file_to_video, file_path, uuid)
+        self.uuid_to_future[uuid] = self.workers.submit(self.driver.process_file_to_video, file_path, uuid)
         return uuid
 
     def process_file_to_video_with_upload(self, file_path: str, job_id: uuid1,
                                           progress_tracker: Callable[[int, float], None] = None) -> Tuple[str, int]:
-        video_path, zipped_file_size = Driver().process_file_to_video(file_path, job_id, progress_tracker)
+        video_path, zipped_file_size = self.driver.process_file_to_video(file_path, job_id, progress_tracker)
 
         def update_upload_progress(progress: int):
             progress_tracker(3, progress)
@@ -56,7 +55,7 @@ class EngineManager:
 
     def process_video_to_file_async(self, video_path: str, compressed_file_size: int) -> uuid1:
         uuid = str(uuid1())
-        self.uuid_to_future[uuid] = self.workers.submit(Driver().process_video_to_file, video_path,
+        self.uuid_to_future[uuid] = self.workers.submit(self.driver.process_video_to_file, video_path,
                                                         compressed_file_size, uuid)
         return uuid
 
@@ -67,10 +66,12 @@ class EngineManager:
                                             progress_tracker: Callable[[int, float], None] = None) -> str:
         def update_download_progress(progress: float):
             progress_tracker(1, progress)
-        downloader: Downloader = DailymotionDownloader(logger=logging.Logger(GENERAL_LOGGER), progress_tracker=update_download_progress)  # choose based on URL
+
+        downloader: Downloader = DailymotionDownloader(logger=logging.Logger(GENERAL_LOGGER),
+                                                       progress_tracker=update_download_progress)  # choose based on URL
         downloaded_video_path = downloader.download(video_url)
-        restored_file_path = Driver().process_video_to_file(
-            downloaded_video_path, compressed_file_size, job_id,progress_tracker
+        restored_file_path = self.driver.process_video_to_file(
+            downloaded_video_path.as_posix(), compressed_file_size, job_id, progress_tracker
         )
         # os.remove(downloaded_video_path)
         return restored_file_path
@@ -102,7 +103,7 @@ class EngineManager:
         results = future.result()
         del self.uuid_to_future[uuid]
         return DailymotionUploader.base_url + results if results else results
-    
+
     def cancel_action(self, uuid):
         future = self.uuid_to_future[uuid]
         future.cancel()
