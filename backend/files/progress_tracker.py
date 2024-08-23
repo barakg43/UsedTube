c50cc50c4e728498f0bb4d4dd448e7781c64f1cd
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-08-23 17:38:27 +0300

    fix phase index error on the last phase

--- backend/files/progress_tracker.py
+++ backend/files/progress_tracker.py
@@ -7,7 +7,7 @@ class ProgressTracker:
         self.progress_array = [0.0 for _ in range(len(phase_weights_array))]
 
     def update_progress(self, phase: int, percentage: float):
-        if phase >= len(self.phase_weights_array):
+        if phase > len(self.phase_weights_array):
             raise IndexError("phase out of bounds")
         self.progress_array[phase - 1] = percentage
         print(f"total :{self.get_total_progress() * 100:.3f}%")
