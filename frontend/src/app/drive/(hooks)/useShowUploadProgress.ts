04af3e484410106f90f008c214dd248dc09fcb69
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-08-30 16:51:27 +0300

    add percent progress in updating toaster

--- frontend/src/app/drive/(hooks)/useShowUploadProgress.ts
+++ frontend/src/app/drive/(hooks)/useShowUploadProgress.ts
@@ -1,56 +1,61 @@
 "use client";
 import { useToaster } from "@/app/(common)/(hooks)/(toaster)/useToaster";
 import {
-    useCancelUploadMutation,
-    useFolderContentQuery,
-    useGetUploadProgressQuery,
+  useCancelUploadMutation,
+  useFolderContentQuery,
+  useGetUploadProgressQuery,
 } from "@/redux/api/driveApi";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { setIsUploading } from "@/redux/slices/fileUploadSlice";
 import { useEffect } from "react";
 
 const useShowUploadProgress = () => {
-    const dispatch = useAppDispatch();
-    const jobId = useAppSelector((state) => state.fileUpload.jobId);
-    const isUploading = useAppSelector((state) => state.fileUpload.isUploading);
+  const dispatch = useAppDispatch();
+  const jobId = useAppSelector((state) => state.fileUpload.jobId);
+  const isUploading = useAppSelector((state) => state.fileUpload.isUploading);
 
-    const { toaster, showProgress } = useToaster();
+  const { toaster, showProgress } = useToaster();
 
-    // Mutation to cancel the upload
-    const [cancelUpload] = useCancelUploadMutation();
+  // Mutation to cancel the upload
+  const [cancelUpload] = useCancelUploadMutation();
 
-    const { refetch } = useFolderContentQuery({
-        folderId: useAppSelector((state) => state.items.activeDirectoryId),
-    });
+  const { refetch } = useFolderContentQuery({
+    folderId: useAppSelector((state) => state.items.activeDirectoryId),
+  });
 
-    const { data } = useGetUploadProgressQuery(
-        { jobId },
-        { skip: !isUploading, pollingInterval: 500 }
-    );
+  const { data } = useGetUploadProgressQuery(
+    { jobId },
+    { skip: !isUploading, pollingInterval: 500 }
+  );
 
-    const onCancel = () => {
-        cancelUpload({ jobId });
-        toaster("Upload cancelled", "info");
+  const onCancel = () => {
+    cancelUpload({ jobId });
+    toaster("Upload cancelled", "info");
+    dispatch(setIsUploading(false));
+  };
+
+  useEffect(() => {
+    if (isUploading && data) {
+      console.log("Uploading:", data);
+      showProgress(
+        jobId,
+        `${new Number(data.progress * 100).toFixed(2)}% Uploading file...`,
+        data.progress,
+        onCancel
+      );
+      if (data.progress === 1) {
+        toaster("Upload complete", "success");
         dispatch(setIsUploading(false));
-    };
+        refetch();
+      }
+    }
+  }, [data, isUploading, jobId]);
 
-    useEffect(() => {
-        if (isUploading && data) {
-            console.log("Uploading:", data);
-            showProgress(jobId, "Uploading file...", data.progress, onCancel);
-            if (data.progress === 1) {
-                toaster("Upload complete", "success");
-                dispatch(setIsUploading(false));
-                refetch();
-            }
-        }
-    }, [data, isUploading, jobId]);
+  const startUploadProgress = () => {
+    dispatch(setIsUploading(true));
+  };
 
-    const startUploadProgress = () => {
-        dispatch(setIsUploading(true));
-    };
-
-    return { startUploadProgress };
+  return { startUploadProgress };
 };
 
 export default useShowUploadProgress;
