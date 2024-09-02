c06536aa36b2a76934ff744829bda29f8dd2ead6
Gal Aviezri <g.aviezri@gmail.com>
2024-09-02 20:51:13 +0300

    rough cut for server download sequence

--- backend/server/files/urls.py
+++ backend/server/files/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('download/<str:file_id>', DownloadView.as_view()),
+    path('download/<str:file_id>', InitiateDownloadView.as_view()),
+    path('download/progress/<str:job_id>', DownloadProgressView.as_view()),
+    
     path('upload/<str:folder_id>', UploadView.as_view()),
     path('used-space', UsedSpaceView.as_view(), name='used_space'),
     path('dir-content/<str:folder_id>', DirectoryContentView.as_view()),
@@ -11,6 +13,5 @@ urlpatterns = [
     path('create-folder', CreateNewFolderView.as_view()),
     path("dir-tree/", DirectoryTree.as_view()),
     path('upload/progress/<str:job_id>', UploadProgressView.as_view()),
-    path('download/progress/<str:job_id>', DownloadViewProgressView.as_view()),
     path('delete/<str:node_id>', DeleteNodeView.as_view()),
 ]
