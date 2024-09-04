a1054575d518ed15c33d54683f4899cf4eb58972
omriSR <omri.1996s@gmail.com>
2024-09-04 20:12:19 +0300

    temp first commit

--- frontend/src/app/drive/(components)/(layout)/(sideBar)/Quota.tsx
+++ frontend/src/app/drive/(components)/(layout)/(sideBar)/Quota.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 const Quota = () => {
+    let how_much_left = 0;
+    let quota_unit = "KB";
+    const file_size = 1024
+    const file_unit = "GB";
+
     return (
         <div className="text-sm flex justify-center pt-4 mt-24 pb-12">
-            0.01 GB used of 1024TB
+            `{how_much_left + quota_unit} used of {file_size + file_unit}`
         </div>
     );
 };
