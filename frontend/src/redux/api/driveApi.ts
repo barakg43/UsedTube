8033e68f087857dbc468abf9d23c5eb750fa579b
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-09-05 19:14:39 +0300

    change initiateDownload to mutation query function

--- frontend/src/redux/api/driveApi.ts
+++ frontend/src/redux/api/driveApi.ts
@@ -3,150 +3,146 @@ import { baseApi } from "../baseApi";
 import { setError, setIsUploading, setJobId } from "../slices/fileUploadSlice";
 
 const driveApiSlice = baseApi.injectEndpoints({
-    endpoints: (builder) => ({
-        // QUERIES
-        // #######
-        folderContent: builder.query({
-            query: ({ folderId }: { folderId: string | undefined }) =>
-                `/files/dir-content/${folderId || ""}`,
-            transformResponse: (response: {
-                files: FileNode[];
-                folders: FSNode[];
-                parents: FSNode[];
-            }) => {
-                return response;
-            },
-        }),
-        sharedItems: builder.query({
-            query: () => ({
-                url: `/sharing/`,
-                method: "GET",
-            }),
-            transformResponse: (response: { files: FileNode[] }) => {
-                if (Array.isArray(response.files))
-                    for (const file of response.files) {
-                        file.type = "file";
-                    }
-                // else response.files/ = "file";
-                return response;
-            },
-        }),
-
-        directoryTree: builder.query({
-            query: () => ({
-                url: "/files/dir-tree/",
-                method: "GET",
-            }),
-        }),
-
-        getUploadProgress: builder.query({
-            query: ({ jobId }: { jobId: string | null }) => ({
-                url: `/files/upload/progress/${jobId}`,
-                method: "GET",
-            }),
-        }),
-
-        initiateDownload: builder.query({
-            query: ({ nodeId }: { nodeId: string }) => ({
-                url: `/files/download/init/${nodeId}`,
-                method: "GET",
-            }),
-        }),
-
-        downloadProgress: builder.query({
-            query: ({ jobId }: { jobId: string }) => ({
-                url: `/files/download/progress/${jobId}`,
-                method: "GET",
-            }),
-        }),
-
-        downloadFile: builder.query({
-            query: ({ jobId }: { jobId: string }) => ({
-                url: `/files/download/${jobId}`,
-                method: "GET",
-            }),
-        }),
-
-        // MUTATIONS
-        // ########
-
-        deleteNode: builder.mutation({
-            query: ({ nodeId }: { nodeId: string }) => ({
-                url: `/files/delete/${nodeId}`,
-                method: "DELETE",
-            }),
-        }),
-
-        deleteSharedNode: builder.mutation({
-            query: ({ nodeId }: { nodeId: string }) => ({
-                url: `/sharing/`,
-                method: "DELETE",
-                body: { nodeId },
-            }),
-        }),
-
-        uploadFile: builder.mutation({
-            query: ({ file, folderId }: { file: File; folderId: string }) => {
-                const formData = new FormData();
-                formData.append("file", file);
-
-                return {
-                    url: `/files/upload/${folderId}`,
-                    method: "POST",
-                    body: formData,
-                };
-            },
-            transformResponse: (response: { job_id: string }) =>
-                response.job_id,
-            async onQueryStarted(
-                { file, folderId },
-                { dispatch, queryFulfilled }
-            ) {
-                try {
-                    const { data } = await queryFulfilled;
-                    //@ts-ignore
-                    dispatch(setJobId(data));
-                } catch (error) {
-                    dispatch(setError("Failed to upload file"));
-                    dispatch(setIsUploading(false));
-                }
-            },
-        }),
-
-        createFolder: builder.mutation({
-            query: ({
-                folderName,
-                parentId,
-            }: {
-                folderName: string;
-                parentId: string;
-            }) => ({
-                url: `/files/create-folder`,
-                method: "POST",
-                body: { folderName, parentId },
-            }),
-        }),
-
-        cancelUpload: builder.mutation({
-            query: ({ jobId }: { jobId: string }) => ({
-                url: `/files/upload/cancel/${jobId}`,
-                method: "DELETE",
-            }),
-        }),
+  endpoints: (builder) => ({
+    // QUERIES
+    // #######
+    folderContent: builder.query({
+      query: ({ folderId }: { folderId: string | undefined }) =>
+        `/files/dir-content/${folderId || ""}`,
+      transformResponse: (response: {
+        files: FileNode[];
+        folders: FSNode[];
+        parents: FSNode[];
+      }) => {
+        return response;
+      },
     }),
+    sharedItems: builder.query({
+      query: () => ({
+        url: `/sharing/`,
+        method: "GET",
+      }),
+      transformResponse: (response: { files: FileNode[] }) => {
+        if (Array.isArray(response.files))
+          for (const file of response.files) {
+            file.type = "file";
+          }
+        // else response.files/ = "file";
+        return response;
+      },
+    }),
+
+    directoryTree: builder.query({
+      query: () => ({
+        url: "/files/dir-tree/",
+        method: "GET",
+      }),
+    }),
+
+    getUploadProgress: builder.query({
+      query: ({ jobId }: { jobId: string | null }) => ({
+        url: `/files/upload/progress/${jobId}`,
+        method: "GET",
+      }),
+    }),
+
+    initiateDownload: builder.mutation({
+      query: ({ nodeId }: { nodeId: string }) => ({
+        url: `/files/download/init/${nodeId}`,
+        method: "GET",
+      }),
+    }),
+
+    downloadProgress: builder.query({
+      query: ({ jobId }: { jobId: string }) => ({
+        url: `/files/download/progress/${jobId}`,
+        method: "GET",
+      }),
+    }),
+
+    downloadFile: builder.query({
+      query: ({ jobId }: { jobId: string }) => ({
+        url: `/files/download/${jobId}`,
+        method: "GET",
+      }),
+    }),
+
+    // MUTATIONS
+    // ########
+
+    deleteNode: builder.mutation({
+      query: ({ nodeId }: { nodeId: string }) => ({
+        url: `/files/delete/${nodeId}`,
+        method: "DELETE",
+      }),
+    }),
+
+    deleteSharedNode: builder.mutation({
+      query: ({ nodeId }: { nodeId: string }) => ({
+        url: `/sharing/`,
+        method: "DELETE",
+        body: { nodeId },
+      }),
+    }),
+
+    uploadFile: builder.mutation({
+      query: ({ file, folderId }: { file: File; folderId: string }) => {
+        const formData = new FormData();
+        formData.append("file", file);
+
+        return {
+          url: `/files/upload/${folderId}`,
+          method: "POST",
+          body: formData,
+        };
+      },
+      transformResponse: (response: { job_id: string }) => response.job_id,
+      async onQueryStarted({ file, folderId }, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          //@ts-ignore
+          dispatch(setJobId(data));
+        } catch (error) {
+          dispatch(setError("Failed to upload file"));
+          dispatch(setIsUploading(false));
+        }
+      },
+    }),
+
+    createFolder: builder.mutation({
+      query: ({
+        folderName,
+        parentId,
+      }: {
+        folderName: string;
+        parentId: string;
+      }) => ({
+        url: `/files/create-folder`,
+        method: "POST",
+        body: { folderName, parentId },
+      }),
+    }),
+
+    cancelUpload: builder.mutation({
+      query: ({ jobId }: { jobId: string }) => ({
+        url: `/files/upload/cancel/${jobId}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
 });
 
 export const {
-    useSharedItemsQuery,
-    useDownloadFileQuery,
-    useDirectoryTreeQuery,
-    useFolderContentQuery,
-    useDownloadProgressQuery,
-    useInitiateDownloadQuery,
-    useGetUploadProgressQuery,
-    useDeleteSharedNodeMutation,
-    useCancelUploadMutation,
-    useCreateFolderMutation,
-    useDeleteNodeMutation,
-    useUploadFileMutation,
+  useSharedItemsQuery,
+  useDownloadFileQuery,
+  useDirectoryTreeQuery,
+  useFolderContentQuery,
+  useDownloadProgressQuery,
+  useInitiateDownloadMutation,
+  useGetUploadProgressQuery,
+  useDeleteSharedNodeMutation,
+  useCancelUploadMutation,
+  useCreateFolderMutation,
+  useDeleteNodeMutation,
+  useUploadFileMutation,
 } = driveApiSlice;
