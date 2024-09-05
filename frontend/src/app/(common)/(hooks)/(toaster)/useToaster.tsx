9c3786da79d4954981de894c0306e778a96e0038
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-09-05 13:14:53 +0300

    fix next error when trying to delete toast

--- frontend/src/app/(common)/(hooks)/(toaster)/useToaster.tsx
+++ frontend/src/app/(common)/(hooks)/(toaster)/useToaster.tsx
@@ -6,48 +6,48 @@ import React from "react";
 export type Variants = "success" | "error" | "warning" | "info";
 
 export function useToaster() {
-    const duration = 3000;
-    const position: ToastPosition = "bottom-right";
+  const duration = 3000;
+  const position: ToastPosition = "bottom-right";
 
-    const toaster = (message: string, type: Variants) => {
-        toast[type](message, { position, autoClose: duration });
-    };
+  const toaster = (message: string, type: Variants) => {
+    toast[type](message, { position, autoClose: duration });
+  };
 
-    const showProgress = (
-        id: string,
-        message: string,
-        progress: number,
-        onCancel: () => void
-    ) => {
-        const content: ToastContent = (
-            <div style={{ display: "flex", alignItems: "center" }}>
-                <CircularProgress variant="determinate" value={progress} />
-                <Typography
-                    variant="body2"
-                    style={{ marginLeft: 10, flexGrow: 1 }}
-                >
-                    {message}
-                </Typography>
-                <IconButton onClick={onCancel} size="small">
-                    <CloseIcon />
-                </IconButton>
-            </div>
-        );
+  const showProgress = (
+    id: string,
+    message: string,
+    progress: number,
+    onCancel?: () => void
+  ) => {
+    const content: ToastContent = (
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <CircularProgress variant='determinate' value={progress} />
+        <Typography variant='body2' style={{ marginLeft: 10, flexGrow: 1 }}>
+          {message}
+        </Typography>
+        <IconButton onClick={onCancel} size='small'>
+          <CloseIcon />
+        </IconButton>
+      </div>
+    );
 
-        if (toast.isActive(id)) {
-            toast.update(id, {
-                render: content,
-                progress: progress,
-                autoClose: false,
-            });
-        } else {
-            toast(content, { toastId: id, position, autoClose: false });
-        }
+    if (toast.isActive(id)) {
+      toast.update(id, {
+        render: content,
+        progress: progress,
+        autoClose: false,
+      });
+    } else {
+      toast(content, { toastId: id, position, autoClose: false });
+    }
 
-        if (progress >= 100) {
-            toast.dismiss(id);
-        }
-    };
+    if (progress >= 100) {
+      toast.update(id, {
+        render: content,
+        autoClose: 3000,
+      });
+    }
+  };
 
-    return { toaster, showProgress };
+  return { toaster, showProgress };
 }
