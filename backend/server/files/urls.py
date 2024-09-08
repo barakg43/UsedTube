77f847df7326638f28fd33113aa52b03b71091b8
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-09-08 16:51:07 +0300

    fix urls in djanago views endpoints

--- backend/server/files/urls.py
+++ backend/server/files/urls.py
@@ -12,10 +12,10 @@ urlpatterns = [
     path('upload/progress/<str:job_id>', UploadProgressView.as_view()),
     path('upload/progress/<str:job_id>/cancel', UploadProgressView.as_view()),
     
-    path('used-space/', UsedSpaceView.as_view(), name='used_space'),
+    path('used-space', UsedSpaceView.as_view(), name='used_space'),
     path('dir-content/<str:folder_id>', DirectoryContentView.as_view()),
-    path('dir-content/', DirectoryContentView.as_view()),
-    path("dir-tree/", DirectoryTree.as_view()),
+    path('dir-content', DirectoryContentView.as_view()),
+    path("dir-tree", DirectoryTree.as_view()),
     
     path('create-folder', CreateNewFolderView.as_view()),
     path('delete/<str:node_id>', DeleteNodeView.as_view()),
