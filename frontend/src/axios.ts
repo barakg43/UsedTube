355ea8a090af44949afb0cc555bece5ce5e2db11
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-09-08 16:56:52 +0300

    fix axios base url to support next js proxy

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
-export const root_api = process.env.NEXT_PUBLIC_HOST;
 export const httpClient = axios.create({
-  baseURL: root_api,
+  baseURL: "/api",
   withCredentials: true,
 });
