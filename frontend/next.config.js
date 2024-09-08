7ed23b4de97b2d6fa401a59be9dcc024f6d3da92
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-09-08 16:56:16 +0300

    use next js rewrites feature to proxy all api request via NextJS server

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,12 @@
+const root_api = process.env.NEXT_PUBLIC_HOST;
 module.exports = {
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${root_api}/:path*`,
+      },
+    ];
+  },
 };
